de61ad21dc0288e6ca05041c1ce61223034646f5
8kdesign <that8kguy@gmail.com>
2021-03-03 20:04:44 +0800

    ModuleList.java: add selected module

---
+++ src/main/java/seedu/duke/module/ModuleList.java
@@ -0,0 +1,26 @@
+package seedu.duke.module;
+
+import seedu.duke.storage.Storage;
+
+import java.util.ArrayList;
+
+public class ModuleList {
+
+    private static final Storage storage = new Storage();
+
+    public static ArrayList<String> moduleList;
+    public static Module selectedModule;
+
+    public static boolean setSelectedModule(String name) {
+        if (!moduleList.contains(name)) {
+            //unable to find
+            return false;
+        }
+        selectedModule = storage.loadModule(name);
+        return true;
+    }
+
+    public static void reset() {
+        selectedModule = null;
+    }
+}
