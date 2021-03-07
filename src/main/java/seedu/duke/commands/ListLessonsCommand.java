82e3fd21fe309513d3d3242531d901fa3a04c591
Hemrish Bundhoo <e0407840@u.nus.edu>
2021-03-07 20:06:00 +0800

    Implemented print list of all lessons

---
+++ src/main/java/seedu/duke/commands/ListLessonsCommand.java
@@ -0,0 +1,29 @@
+package seedu.duke.commands;
+
+import seedu.duke.lesson.Lesson;
+import seedu.duke.module.Module;
+import seedu.duke.module.ModuleList;
+import seedu.duke.ui.UI;
+
+import java.util.ArrayList;
+
+public class ListLessonsCommand extends Command {
+    public static final String PRINT_LESSONS_FORMAT = "%d. %s - %s\n \t%s\n \t%s\n \t%s";
+
+    public ListLessonsCommand() {
+        System.out.println("Lessons for CS2113T:");
+    }
+
+    @Override
+    public void execute(ModuleList moduleList, UI ui) {
+        Module module = ModuleList.getSelectedModule();
+        ArrayList<Lesson> lessonList = module.getLessonList();
+        int counter = 1;
+        for (Lesson lesson : lessonList) {
+            System.out.println(String.format(PRINT_LESSONS_FORMAT, counter,
+                    AddLessonCommand.getLessonTypeString(lesson.getLessonType()), lesson.getTime(),
+                    lesson.getOnlineLink(), lesson.getTeachingStaff().getName(), lesson.getTeachingStaff().getEmail()));
+            counter++;
+        }
+    }
+}
