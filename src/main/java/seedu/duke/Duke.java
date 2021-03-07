7974023b547abdb8b80d26ae6566bad9faf7a122
Lee Yang Peng <lee.yangpeng@dhs.sg>
2021-03-07 16:54:08 +0800

    no message

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -5,18 +5,34 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
 
-        System.out.println("Hello! I'm FridgeFriend!");
-        System.out.println("What can I do for you?\n");
+        welcomeMessage();
 
         String input;
         Scanner in = new Scanner(System.in);
         input = in.nextLine();
-        while (!input.equals("bye")){
-            System.out.println(input + "\n");
-            input = in.nextLine();
+
+        boolean isBye = false;
+        while (!isBye) {
+            switch (input) {
+            case "bye":
+                isBye = true;
+                byeMessage();
+            default:
+                System.out.println(input + "\n");
+                input = in.nextLine();
+            }
         }
 
-        System.out.println("Bye. Hope to see you again soon!\n");
 
+
+    }
+
+    private static void byeMessage() {
+        System.out.println("Bye. Hope to see you again soon!\n");
+    }
+
+    private static void welcomeMessage() {
+        System.out.println("Hello! I'm FridgeFriend!");
+        System.out.println("What can I do for you?\n");
     }
 }
