128e16d5a7ac4c789b7e5a89ac907f7fb5e79e60
LeeHanYongAndy <andyleehy2502@gmail.com>
2021-03-07 21:15:10 +0800

    Add skeletons
    Fixes #17

---
+++ src/main/java/seedu/duke/common/Constant.java
@@ -0,0 +1,14 @@
+package seedu.duke.common;
+
+public class Constant {
+    // Please append constants used to line under your name
+    // Mark
+
+    // Andy
+
+    // Jonah
+
+    // Gerard
+
+    // Jason
+}
