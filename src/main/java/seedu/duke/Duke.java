d688cb54b2469763838021f4cbcdb94fdebb8e3a
Jo-Nah <jonahtham@u.nus.edu>
2021-03-08 00:27:58 +0800

    Rough skeleton of Ui

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,12 +1,17 @@
 package seedu.duke;
 
+import seedu.duke.ui.Ui;
+
 import java.util.Scanner;
 
 public class Duke {
+    private Ui ui;
+
     /**
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
+        /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -17,5 +22,31 @@ public class Duke {
 
         Scanner in = new Scanner(System.in);
         System.out.println("Hello " + in.nextLine());
+         */
+        new Duke().run();
+    }
+
+    /** Runner for the FINUX Application */
+    private void run() {
+        start();
+        commandLooper();
+        end();
+    }
+
+    private void start() {
+        this.ui = new Ui();
+        ui.printWelcomeMessage();
+    }
+
+    private void end() {
+        ui.printGoodByeMessage();
+    }
+
+    private void commandLooper() {
+        String rawInput;
+        do {
+            rawInput = ui.getUserInput();
+            System.out.println("You have entered: " + rawInput);
+        } while(!rawInput.equals("exit"));
     }
 }
