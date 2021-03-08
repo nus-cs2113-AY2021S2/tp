29e3afa379747c443bf8307f5d1cfb2096cbc0ae
tzexern <tantzexern@gmail.com>
2021-03-08 23:18:10 +0800

    Update RecordHandler, Loan and Expense
    Fixes #13

--- src/main/java/seedu/duke/record/Expense.java
+++ src/main/java/seedu/duke/record/Expense.java
@@ -1,4 +1,7 @@
 package seedu.duke.record;
 
-public class Expense {
+public class Expense extends Record {
+    public Expense(double amount, String issuedDate, String description) {
+        super(amount, issuedDate, description);
+    }
 }
