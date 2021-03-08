6dabbc7097118c7967abae8f98a54b4469d06d70
SimJJ96 <simjingjie13@gmail.com>
2021-03-08 17:08:57 +0800

    Add method to add food
    Fixes #17

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,32 +1,39 @@
 package seedu.duke;
 
+import seedu.duke.exception.EmptyFoodDescriptionException;
+import seedu.duke.exception.EmptyInputException;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+
+    private static final List<String> foodList = new ArrayList<>();
+
+    private static final int COMMAND_WORD = 0;
+
+    private static boolean isBye = false;
+
     public static void main(String[] args) {
 
         printWelcomeMessage();
 
         String input;
         Scanner in = new Scanner(System.in);
-        input = in.nextLine();
 
-        boolean isBye = false;
         while (!isBye) {
-            switch (input.toLowerCase()) {
-            case "bye":
-                isBye = true;
-                printByeMessage();
-                break;
-            default:
-                System.out.println(input + "\n");
-                input = in.nextLine();
-                break;
+            try {
+                input = getUserInput(in);
+                parseInput(input);
+            } catch (Exception exception) {
+                printErrorMessage(exception);
             }
         }
+    }
 
-
-
+    private static String getUserInput(Scanner in) {
+        return in.nextLine();
     }
 
     private static void printByeMessage() {
@@ -37,4 +44,64 @@ public class Duke {
         System.out.println("Hello! I'm FridgeFriend!");
         System.out.println("What can I do for you?\n");
     }
+
+    private static void addFoodIntoList(String foodDescription) {
+        foodList.add(foodDescription);
+    }
+
+    private static void checkFoodDescription(String foodDescription) throws EmptyFoodDescriptionException {
+        if (foodDescription.isEmpty()) {
+            throw new EmptyFoodDescriptionException();
+        }
+    }
+
+    private static void printAddFoodMessage(String foodDescription) {
+        System.out.println("Great! I have added [" + foodDescription + "] into your fridge.\n");
+    }
+
+    private static void printErrorMessage(Exception exception) {
+        if (exception instanceof EmptyInputException) {
+            System.out.println("Sorry my friend, please give a valid input.\n");
+        } else if (exception instanceof EmptyFoodDescriptionException) {
+            System.out.println("Sorry my friend, the description cannot be empty.\n");
+        }
+    }
+
+    //@@author SimJJ96-reused
+    /*Reused from https://github.com/SimJJ96/ip/blob/master/src/
+            main/java/duke/Parser.java with minor modification*/
+    /**
+     * Parses input into command and description and execute.
+     *
+     * @param input user input string
+     * @throws EmptyInputException if the initial input is empty
+     * @throws EmptyFoodDescriptionException if the description of food is empty
+     */
+    public static void parseInput(String input) throws EmptyInputException,
+            EmptyFoodDescriptionException {
+
+        if (input.isEmpty()) {
+            throw new EmptyInputException();
+        }
+
+        String[] words = input.trim().split("\\s+", 2);
+        String command = words[COMMAND_WORD];
+        String foodDescription = input.replaceFirst(command,"").trim();
+
+        switch (command.toLowerCase()) {
+        case "add":
+            checkFoodDescription(foodDescription);
+            addFoodIntoList(foodDescription);
+            printAddFoodMessage(foodDescription);
+            break;
+        case "bye":
+            isBye = true;
+            printByeMessage();
+            break;
+        default:
+            System.out.println(command + " " + foodDescription + "\n");
+            break;
+        }
+    }
+    //@author
 }
---
+++ src/main/java/seedu/duke/exception/EmptyInputException.java
@@ -0,0 +1,4 @@
+package seedu.duke.exception;
+
+public class EmptyInputException extends Exception {
+}
