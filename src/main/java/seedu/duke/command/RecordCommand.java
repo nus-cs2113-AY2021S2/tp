053089560ae53db93a56d09ca7fe7caa88101f27
Brandon Foong <brandonfoongyankai@gmail.com>
2021-03-09 21:01:05 +0800

    Add the classes for the `record` and `retrieve` commands

---
+++ src/main/java/seedu/duke/command/RecordCommand.java
@@ -0,0 +1,24 @@
+package seedu.duke.command;
+
+import seedu.duke.Data;
+import seedu.duke.Ui;
+
+import java.util.HashMap;
+
+public class RecordCommand extends Command{
+    /**
+     * This is the constructor of the command. Arguments are passed to parent class.
+     * @param ui Instance of Ui class, for UI input/output
+     * @param data Instance of Data class, for manipulating patient list and read/write miscallaneous config
+     * @param arguments Arguments decomposed from the full command given by the user
+     */
+    public RecordCommand(Ui ui, Data data, HashMap<String, String> arguments) {
+        super(ui, data, arguments);
+    }
+
+    @Override
+    public void execute() {
+        // Replace by ui after ui is implemented
+        System.out.println("Dummy Record Command");
+    }
+}
---
+++ src/main/java/seedu/duke/command/RetrieveCommand.java
@@ -0,0 +1,24 @@
+package seedu.duke.command;
+
+import seedu.duke.Data;
+import seedu.duke.Ui;
+
+import java.util.HashMap;
+
+public class RetrieveCommand extends Command{
+    /**
+     * This is the constructor of the command. Arguments are passed to parent class.
+     * @param ui Instance of Ui class, for UI input/output
+     * @param data Instance of Data class, for manipulating patient list and read/write miscallaneous config
+     * @param arguments Arguments decomposed from the full command given by the user
+     */
+    public RetrieveCommand(Ui ui, Data data, HashMap<String, String> arguments) {
+        super(ui, data, arguments);
+    }
+
+    @Override
+    public void execute() {
+        // Replace by ui after ui is implemented
+        System.out.println("Dummy Retrieve Command");
+    }
+}
