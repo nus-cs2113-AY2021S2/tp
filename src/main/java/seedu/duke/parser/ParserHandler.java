3185f5c2bd3761ef07682811028dea155162433f
Mark Low <nedgangk@gmail.com>
2021-03-09 18:08:50 +0800

    Testing IO for list command

--- src/main/java/seedu/duke/parser/ParserHandler.java
+++ src/main/java/seedu/duke/parser/ParserHandler.java
@@ -19,15 +19,26 @@ public class ParserHandler {
             trimmedInput = trimmedInput.substring(2);
             optionIndex = getNextOptionIndex(trimmedInput);
         }
-        extracted.add(trimmedInput.trim());
+        if (checkOptionEndWith(trimmedInput.stripLeading())) {
+            extracted.add(trimmedInput.substring(0, trimmedInput.length() - 2).trim());
+            trimmedInput = trimmedInput.substring(trimmedInput.length() - 2);
+            extracted.add(trimmedInput);
+            extracted.add("");
+        } else {
+            extracted.add(trimmedInput.trim());
+        }
         return extracted;
     }
 
     public static boolean checkOptionStartWith(String input) {
-        return StringUtils.startsWithAny(input, "-l ", "-s ", "-d ", "-c ");
+        return StringUtils.startsWithAny(input, "-e", "-l", "-s", "-d", "-a");
+    }
+
+    public static boolean checkOptionEndWith(String input) {
+        return  StringUtils.endsWithAny(input, " -e", " -l", " -s", " -d", " -a");
     }
 
     public static int getNextOptionIndex(String leftOverString) {
-        return StringUtils.indexOfAny(leftOverString, " -e ", " -l ", " -s ", " -d ", " -c ");
+        return StringUtils.indexOfAny(leftOverString, " -e ", " -l ", " -s ", " -d ", " -a ");
     }
 }
