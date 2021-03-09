a5b4063ec2bf1bfd898dd8ea8e1923b2b208778d
Huachen <huachenw24@gmail.com>
2021-03-09 15:07:18 +0800

    fix spacing for welcome message

--- src/main/java/seedu/duke/messages/Messages.java
+++ src/main/java/seedu/duke/messages/Messages.java
@@ -7,8 +7,8 @@ public class Messages {
             + " | |    / _ \\| '_ \\| '_ \\ / _ \\| / __/ __|/ _ \\ | | | '__|\n"
             + " | |___| (_) | | | | | | | (_) | \\__ \\__ \\  __/ |_| | |   \n"
             + "  \\_____\\___/|_| |_|_| |_|\\___/|_|___/___/\\___|\\__,_|_|   \n";
-            
-    public static final String WELCOME_MESSAGE = "Welcome to"
+
+    public static final String WELCOME_MESSAGE = "                         Welcome to\n"
             + LOGO
-            + "Waiting for command...";
+            + "\n                     Waiting for command...";
 }
