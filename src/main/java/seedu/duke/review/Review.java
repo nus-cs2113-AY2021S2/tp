1268742760d7ae66c85a10f0dd4b92def1ae0fc9
Huachen <huachenw24@gmail.com>
2021-03-09 14:58:51 +0800

    add skeleton review class

---
+++ src/main/java/seedu/duke/review/Review.java
@@ -0,0 +1,11 @@
+package seedu.duke.review;
+
+import java.time.LocalDate;
+
+public class Review {
+    public String title;
+    public String category;
+    public String description;
+    public int stars;
+    public LocalDate date;
+}
