0b72db0f374be3f7b8ed53e35de28692fb3cde4c
Rye98 <zhong.shuhao@icloud.com>
2021-03-09 19:12:26 +0800

    Added Record class

---
+++ src/main/java/seedu/duke/Record.java
@@ -0,0 +1,12 @@
+package seedu.duke;
+
+import java.util.ArrayList;
+
+public class Record {
+
+    private ArrayList<String> records;
+
+    public Record() {
+        records = new ArrayList<String>();
+    }
+}
