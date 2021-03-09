6284c3e1d375a6c0fd4cd4014bcc2f0d3a8f6f28
KevinNg <WNG052@e.ntu.edu.sg>
2021-03-09 21:43:17 +0800

    Merged upstream

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -11,15 +11,23 @@ public class Duke {
     private static ArrayList<Canteen> canteens; // todo: add a canteen manager
     private static Ui ui;
     private static Storage storage;
-    private static final String filePath = "./src/main/java/data/storage.txt";
+    private static final String filePath = "data/storage.txt";
 
     /**
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
+//        System.out.println(args[0]);
+//        System.out.println("Working Directory = " + System.getProperty("user.dir"));
         ui = new Ui();
         ui.showWelcome();
-        storage = new Storage(filePath);
+//        storage = new Storage(filePath);
+        if (args != null & args.length > 0) {
+            storage = new Storage(args[0]);
+        } else {
+            storage = new Storage(filePath);
+        }
+//        storage = new Storage(args[0]);
         canteens = storage.load();
         echo();
     }
@@ -38,6 +46,7 @@ public class Duke {
             }
         }
         ui.showGoodbye();
+        System.exit(0);
     }
 
     public static void displayStores() {
