1c3fdde037613e9f51a144b495f1e5099f05bd8c
Rye98 <zhong.shuhao@icloud.com>
2021-03-09 19:13:11 +0800

    Added displayRecord Method
    Fixes #19
    Fixes #20

--- src/main/java/seedu/duke/Record.java
+++ src/main/java/seedu/duke/Record.java
@@ -9,4 +9,19 @@ public class Record {
     public Record() {
         records = new ArrayList<String>();
     }
+
+    public ArrayList<String> getRecords() {
+        return records;
+    }
+
+    public void displayRecords() {
+        /**
+         * Lists out all the entries in records.
+         */
+        int i = 1;
+        for (String record : records) {
+            System.out.println(i + ". " + record);
+            i++;
+        }
+    }
 }
