95b2bf671dd72460481563c42d0cf1307d1df1ac
LeeHanYongAndy <andyleehy2502@gmail.com>
2021-03-09 18:13:20 +0800

    Reimplement parser with proper logic flow. Need to refactor.

--- src/main/java/seedu/duke/parser/ParserHandler.java
+++ src/main/java/seedu/duke/parser/ParserHandler.java
@@ -11,23 +11,39 @@ public class ParserHandler {
             extracted.add(trimmedInput.substring(0,2));
             trimmedInput = trimmedInput.substring(2);
         }
-        int optionIndex = getNextOptionIndex(trimmedInput);
-        while (optionIndex != -1) {
-            extracted.add(trimmedInput.substring(0,optionIndex).trim());
-            trimmedInput = trimmedInput.substring(optionIndex).stripLeading();
-            extracted.add(trimmedInput.substring(0,2));
-            trimmedInput = trimmedInput.substring(2);
-            optionIndex = getNextOptionIndex(trimmedInput);
+        String argument;
+        while (true) {
+            int optionIndex = getNextOptionIndex(trimmedInput);
+            if (optionIndex != -1) {
+                argument = trimmedInput.substring(0,optionIndex).trim();
+                extracted.add(argument);
+                trimmedInput = trimmedInput.substring(optionIndex).stripLeading();
+                extracted.add(trimmedInput.substring(0,2));
+                trimmedInput = trimmedInput.substring(2);
+            } else {
+                if (checkOptionEndWith(trimmedInput)) {
+                    argument = trimmedInput.substring(0,trimmedInput.length()-2).trim();
+                    extracted.add(argument);
+                    extracted.add(trimmedInput.substring(trimmedInput.length()-2).trim());
+                    extracted.add("");
+                } else {
+                    extracted.add(trimmedInput.trim());
+                }
+                break;
+            }
         }
-        extracted.add(trimmedInput.trim());
         return extracted;
     }
 
     public static boolean checkOptionStartWith(String input) {
-        return StringUtils.startsWithAny(input, "-l ", "-s ", "-d ", "-c ");
+        return StringUtils.startsWithAny(input, "-e", "-l", "-s", "-d", "-a");
+    }
+
+    public static boolean checkOptionEndWith(String input) {
+        return  StringUtils.endsWithAny(input, " -e", " -l", " -s", " -d", " -a");
     }
 
     public static int getNextOptionIndex(String leftOverString) {
-        return StringUtils.indexOfAny(leftOverString, " -e ", " -l ", " -s ", " -d ", " -c ");
+        return StringUtils.indexOfAny(leftOverString, " -e ", " -l ", " -s ", " -d ", " -a ");
     }
 }
