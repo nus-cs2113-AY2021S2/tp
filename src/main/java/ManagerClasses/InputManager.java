309d6ba03aa58cc919c2bac79d1b97141929378e
Rizavur Rahman <rizavur@gmail.com>
2021-03-10 19:49:30 +0800

    Integrate routing and history commands

--- src/main/java/ManagerClasses/InputManager.java
+++ src/main/java/ManagerClasses/InputManager.java
@@ -1,5 +1,8 @@
 package ManagerClasses;
 
+import seedu.duke.Record;
+import seedu.duke.Routing;
+
 import java.util.Scanner;
 
 public class InputManager {
@@ -15,19 +18,28 @@ public class InputManager {
         while(true) {
             String input = getUserInput();
             CommandManager commandManager = new CommandManager(input);
+            Routing map = new Routing();
+            Record history = new Record();
 
             switch (commandManager.getCommandType()) {
             case GO:
+                map.executeRouting();
+                // todo: add records
                 break;
             case HISTORY:
+                history.displayRecords();
                 break;
             case CLEARHISTORY:
+                history.emptyRecords();
                 break;
             case ADDNOTE:
+                System.out.println("Add note to the selected location");
                 break;
             case DELETENOTE:
+                System.out.println("Delete note from selected location");
                 break;
             case DISPLAYNOTES:
+                System.out.println("Display notes of selected location");
                 break;
             case BYE:
                 UIManager.showByeMessage();
