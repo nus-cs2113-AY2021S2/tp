9c94b3e7cc73bd2139c9f4d206459bc86df19f30
Rizavur Rahman <rizavur@gmail.com>
2021-03-10 19:45:51 +0800

    Add ability for input manager to obtain and execute commands

--- src/main/java/ManagerClasses/InputManager.java
+++ src/main/java/ManagerClasses/InputManager.java
@@ -1,5 +1,41 @@
 package ManagerClasses;
 
+import java.util.Scanner;
+
 public class InputManager {
-    
+    private String getUserInput() {
+        System.out.print("> ");
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        UIManager.printDivider();
+        return input;
+    }
+
+    public void inputLoop() {
+        while(true) {
+            String input = getUserInput();
+            CommandManager commandManager = new CommandManager(input);
+
+            switch (commandManager.getCommandType()) {
+            case GO:
+                break;
+            case HISTORY:
+                break;
+            case CLEARHISTORY:
+                break;
+            case ADDNOTE:
+                break;
+            case DELETENOTE:
+                break;
+            case DISPLAYNOTES:
+                break;
+            case BYE:
+                UIManager.showByeMessage();
+                return;
+            default:
+                UIManager.showInvalidMessage();
+            }
+            UIManager.printDivider();
+        }
+    }
 }
