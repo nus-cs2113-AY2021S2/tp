b3360332a0f20afe030ee11ada2dabad5520047a
Rizavur Rahman <rizavur@gmail.com>
2021-03-10 19:39:46 +0800

    Add ability to convert user input to command

--- src/main/java/ManagerClasses/CommandManager.java
+++ src/main/java/ManagerClasses/CommandManager.java
@@ -1,5 +1,42 @@
 package ManagerClasses;
 
 public class CommandManager {
+    private CommandEnum commandType;
 
+    public CommandManager(String input) {
+        setCommandFromInput(input);
+    }
+
+    public CommandEnum getCommandType() {
+        return commandType;
+    }
+
+    private void setCommandFromInput(String input) {
+        // Trim input to remove unnecessary whitespaces
+        // Convert input to lowercase to make command case insensitive
+        String filteredInput = input.trim().toLowerCase();
+
+        commandType = getCommandTypeFromInput(filteredInput);
+    }
+
+    private CommandEnum getCommandTypeFromInput(String filteredInput) {
+        switch (filteredInput) {
+        case "go":
+            return CommandEnum.GO;
+        case "history":
+            return CommandEnum.HISTORY;
+        case "clear history":
+            return CommandEnum.CLEARHISTORY;
+        case "add note":
+            return CommandEnum.ADDNOTE;
+        case "delete note":
+            return CommandEnum.DELETENOTE;
+        case "notes":
+            return CommandEnum.DISPLAYNOTES;
+        case "bye":
+            return CommandEnum.BYE;
+        default:
+            return CommandEnum.INVALID;
+        }
+    }
 }
