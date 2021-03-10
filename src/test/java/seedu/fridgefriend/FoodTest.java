f9b8504043a5c1e9f52fff3ab99c6c0af30d79e2
Lee Yang Peng <lee.yangpeng@dhs.sg>
2021-03-10 22:04:16 +0800

    Improved Test Cases for JUnit Tests

--- src/test/java/seedu/fridgefriend/FoodTest.java
+++ src/test/java/seedu/fridgefriend/FoodTest.java
@@ -3,6 +3,7 @@ package seedu.fridgefriend;
 import org.junit.jupiter.api.Test;
 import seedu.fridgefriend.food.Food;
 import seedu.fridgefriend.food.FoodCategory;
+import seedu.fridgefriend.food.FoodStorageLocation;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -13,5 +14,43 @@ public class FoodTest {
         Food pork = new Food(FoodCategory.MEAT, "pork");
         assertEquals(FoodCategory.MEAT, pork.getCategory());
         assertEquals("pork", pork.getFoodName());
+    }
+
+    @Test
+    void testStorageExpiryDate() {
+        Food beef = new Food(FoodCategory.MEAT, "beef");
+        beef.setStorageDate("10 March 2021");
+        beef.setExpiryDate("15 March 2021");
+        assertEquals("10 March 2021", beef.getStorageDate());
+        assertEquals("15 March 2021", beef.getExpiryDate());
+    }
+
+    @Test
+    void testStorageLocation() {
+        Food grouper = new Food(FoodCategory.SEAFOOD, "grouper");
+        grouper.setStorageLocation(FoodStorageLocation.FREEZER);
+        assertEquals(FoodStorageLocation.FREEZER, grouper.getStorageLocation());
+    }
+
+    @Test
+    void testChangeFoodDescriptionCategory() {
+        Food chocolate = new Food(FoodCategory.DAIRY, "chocolate");
+        assertEquals(FoodCategory.DAIRY, chocolate.getCategory());
+        assertEquals("chocolate", chocolate.getFoodName());
+        chocolate.setFoodName("melted chocolate");
+        chocolate.setCategory(FoodCategory.OTHER);
+        assertEquals(FoodCategory.OTHER, chocolate.getCategory());
+        assertEquals("melted chocolate", chocolate.getFoodName());
+    }
+
+    @Test
+    void testNewFoodAllParameters() {
+        Food eggs = new Food(FoodCategory.EGG, "eggs", "11 March 2021",
+                "20 March 2021", FoodStorageLocation.UPPER_SHELF);
+        assertEquals(FoodCategory.EGG, eggs.getCategory());
+        assertEquals("eggs", eggs.getFoodName());
+        assertEquals("11 March 2021", eggs.getStorageDate());
+        assertEquals("20 March 2021", eggs.getExpiryDate());
+        assertEquals(FoodStorageLocation.UPPER_SHELF, eggs.getStorageLocation());
     }
 }
