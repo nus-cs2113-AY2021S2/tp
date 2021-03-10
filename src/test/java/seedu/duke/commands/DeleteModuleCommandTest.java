70746b12b9dc2d8b28aadb9f9f113e0350286231
isaharon <isabharon@gmail.com>
2021-03-10 17:36:06 +0800

    Add test files for module commands

---
+++ src/test/java/seedu/duke/commands/DeleteModuleCommandTest.java
@@ -0,0 +1,51 @@
+package seedu.duke.commands;
+
+import org.junit.jupiter.api.Test;
+import seedu.duke.common.Messages;
+import seedu.duke.exceptions.CommandException;
+import seedu.duke.module.ModuleList;
+import seedu.duke.ui.UI;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DeleteModuleCommandTest {
+    private final PrintStream originalOut = System.out;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+
+    @Test
+    void execute_validModuleNumbersInput_expectSuccess() throws CommandException {
+        ByteArrayInputStream inContent = new ByteArrayInputStream("1 3".getBytes());
+        System.setOut(new PrintStream(outContent));
+        System.setIn(inContent);
+
+        ModuleList modules = new ModuleList();
+        modules.addModule("CS2113T");
+        modules.addModule("CS2101");
+        modules.addModule("CS2105");
+        modules.addModule("CS2106");
+
+        Command command = new DeleteModuleCommand();
+        command.execute(modules, new UI());
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(DeleteModuleCommand.MESSAGE_PROMPT1);
+        sb.append(String.format(Messages.FORMAT_LIST_ITEMS, 1, "CS2113T")).append("\n");
+        sb.append(String.format(Messages.FORMAT_LIST_ITEMS, 2, "CS2101")).append("\n");
+        sb.append(String.format(Messages.FORMAT_LIST_ITEMS, 3, "CS2105")).append("\n");
+        sb.append(String.format(Messages.FORMAT_LIST_ITEMS, 4, "CS2106")).append("\n");
+        sb.append("\n");
+        sb.append(DeleteModuleCommand.MESSAGE_PROMPT2);
+        sb.append(System.lineSeparator());
+        sb.append(String.format(DeleteModuleCommand.MESSAGE_SUCCESS, "CS2113T")).append("\n");
+        sb.append(String.format(DeleteModuleCommand.MESSAGE_SUCCESS, "CS2105")).append("\n");
+
+        assertEquals(sb.toString() + System.lineSeparator(), outContent.toString());
+
+        System.setOut(originalOut);
+    }
+}
---
+++ src/test/java/seedu/duke/commands/ExitProgramCommandTest.java
@@ -0,0 +1,40 @@
+package seedu.duke.commands;
+
+import org.junit.jupiter.api.Test;
+import seedu.duke.common.Messages;
+import seedu.duke.exceptions.CommandException;
+import seedu.duke.module.ModuleList;
+import seedu.duke.ui.UI;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ExitProgramCommandTest {
+    private final PrintStream originalOut = System.out;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Test
+    void execute_noInput_expectMessageExit() throws CommandException {
+        System.setOut(new PrintStream(outContent));
+
+        ModuleList modules = new ModuleList();
+        modules.addModule("CS2105");
+        modules.addModule("CS2106");
+
+        Command command = new ExitProgramCommand();
+        command.execute(modules, new UI());
+
+        String output = Messages.MESSAGE_EXIT;
+        assertEquals(output + System.lineSeparator(), outContent.toString());
+
+        System.setOut(originalOut);
+    }
+
+    @Test
+    void isExit() {
+        assertTrue(new ExitProgramCommand().isExit());
+    }
+}
