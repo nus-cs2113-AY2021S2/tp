54db9c0d504758aed519b09f894d97dc85f4b5e0
nivikcivik <nikhila.ravi@hotmail.com>
2021-03-11 20:35:27 +0800

    created JUnit tests for (and edited) deleteModule

---
+++ src/test/java/seedu/duke/ModuleInfoTest.java
@@ -0,0 +1,22 @@
+package seedu.duke;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ModuleInfoTest {
+
+    @Test
+    void isValidModuleNumber_invalidModuleNumber_throwsNumberFormatException() {
+        assertThrows(NumberFormatException.class, () -> {
+            ModuleInfo.checkValidModuleToDelete("A");
+        });
+    }
+
+    @Test
+    void isValidModuleNumber_invalidModuleNumber_throwsIndexOutOfBoundsException() {
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            ModuleInfo.checkValidModuleToDelete("-1");
+        });
+    }
+}
