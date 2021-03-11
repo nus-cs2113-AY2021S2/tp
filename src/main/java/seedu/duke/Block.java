b7334eebe0adaa0cbc945eac1fa569dec7dc7f0c
wjchoi0712 <e0376605@u.nus.edu>
2021-03-11 09:07:23 +0800

    Fixed block class

--- src/main/java/seedu/duke/Block.java
+++ src/main/java/seedu/duke/Block.java
@@ -24,26 +24,4 @@ public class Block {
     public boolean isSameBlock(Block block) {
         return this.name.equals(block.name);
     }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + name.hashCode();;
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Block other = (Block) obj;
-        if (name != other.name)
-            return false;
-        return true;
-    }
 }
