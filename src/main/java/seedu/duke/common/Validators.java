da39cdd813387fbc03634a25f5fd2db578e2d3e0
LeeHanYongAndy <andyleehy2502@gmail.com>
2021-03-11 17:34:02 +0800

    Add validateDate methods in Validators.java, Add exception catching in add command.

--- src/main/java/seedu/duke/common/Validators.java
+++ src/main/java/seedu/duke/common/Validators.java
@@ -1,16 +1,22 @@
 package seedu.duke.common;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Validators {
 
     public static void validateIndex() {
 
     }
 
-    public static void validateDate() {
-
+    public static boolean validateDate(String inputToCheck) {
+        if (inputToCheck.contains("dummy")) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public static void validateAmount() {
-
+    public static boolean validateAmount(String inputToCheck) {
+        return StringUtils.isNumeric(inputToCheck);
     }
 }
