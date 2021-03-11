1a658142830030e328868edcfd68a0c46a201f23
CabbageTime <cabbagetime@outlook.com>
2021-03-11 15:02:39 +0800

    Change coding so that '+' appears on new line.

--- src/main/java/seedu/hdbuy/data/Unit.java
+++ src/main/java/seedu/hdbuy/data/Unit.java
@@ -49,7 +49,7 @@ public class Unit {
     }
 
     @Override public String toString() {
-        return "Hash ID: " + getId() + " - Address: " + getAddress() + " - Type: " + getType() + " - Lease: " +
-                getLease() + " -  Price: $" + getPrice();
+        return "Hash ID: " + getId() + " - Address: " + getAddress() + " - Type: " + getType() + " - Lease: "
+                + getLease() + " -  Price: $" + getPrice();
     }
 }
