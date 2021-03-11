7c2fa4a7c817ce8061495f7f22232ba85417ebcd
gerardtwk <gerardtwk@gmail.com>
2021-03-11 16:51:59 +0800

    Refactored Duke class to reflect the style presented in A-MoreOOP

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -17,42 +17,18 @@ public class Duke {
     private RecordList records;
     private Storage storage;
 
-    /**
-     * Main entry-point for the java.duke.Duke application.
-     */
-    public static void main(String[] args) {
-        /*
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("What is your name?");
-
-        Scanner in = new Scanner(System.in);
-        System.out.println("Hello " + in.nextLine());
-         */
-        new Duke().run();
+    public Duke() {
+        ui = new Ui();
+        records = new RecordList();
+        storage = new Storage();
     }
 
     /**
      * Runner for the FINUX Application.
      */
     private void run() {
-        start();
-        commandLooper();
-        end();
-    }
-
-    private void start() {
-        ui = new Ui();
-        records = new RecordList();
-        storage = new Storage();
         ui.printWelcomeMessage();
-    }
-
-    private void end() {
+        commandLooper();
         //ui.printGoodByeMessage();
         System.out.println("PROGRAM TERMINATES HERE!");
     }
@@ -62,17 +38,22 @@ public class Duke {
         String rawInput;
         do {
             rawInput = ui.getUserInput();
-            //System.out.println("You have entered: " + rawInput);
             ArrayList<String> parsedStringList = ParserHandler.getParseInput(rawInput);
-            //System.out.println("You have entered: " + parsedStringList);
             command = parseCommand(parsedStringList);
-            if (command == null) {
-                continue;
+            if (command != null) {
+                command.execute(records, ui, storage);
             }
-            command.execute(records, ui, storage);
         } while (!ExitCommand.isExit(command));
     }
 
+    /**
+     * Main entry-point for the java.duke.Duke application.
+     */
+    public static void main(String[] args) {
+        new Duke().run();
+    }
+
+    //Shift to ParserHandler class
     private Command parseCommand(ArrayList<String> parsedString) {
         try {
             Command type = CommandHandler.createCommand(parsedString);
