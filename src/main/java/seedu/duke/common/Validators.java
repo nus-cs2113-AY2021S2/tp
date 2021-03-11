abbf13eded2703cac97ce91a928acb3a6ef897fb
tzexern <tantzexern@gmail.com>
2021-03-11 15:43:29 +0800

    Implement removeCommand class, Add InvalidIndexException
    Fixes #32

---
+++ src/main/java/seedu/duke/common/Validators.java
@@ -0,0 +1,16 @@
+package seedu.duke.common;
+
+public class Validators {
+
+    public static void validateIndex() {
+
+    }
+
+    public static void validateDate() {
+
+    }
+
+    public static void validateAmount() {
+
+    }
+}
