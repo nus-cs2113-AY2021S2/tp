26a69a819c2495c2fa84f05065969cf2165ec4d2
Song Yu <song0180@e.ntu.edu.sg>
2021-03-12 19:33:20 +0800

    TP-0: Fix CI error by adjusting code format

--- src/main/java/seedu/duke/ui/UI.java
+++ src/main/java/seedu/duke/ui/UI.java
@@ -5,24 +5,24 @@ public class UI {
     public static final String DIVIDER = "------------------------------------------------\n";
 
     public static void printGreetings() {
-        System.out.println(DIVIDER +
-                "Welcome to your personal fitness app - Healthier\n" +
-                "What's in your mind today?\n" +
-                DIVIDER);
+        System.out.println(DIVIDER
+                + "Welcome to your personal fitness app - Healthier\n"
+                + "What's in your mind today?\n"
+                + DIVIDER);
     }
 
     public static void printExitMessage() {
-        System.out.println(DIVIDER +
-                "Nice work today!\n" +
-                "You are one step closer to ultimate fitness!\n" +
-                "See you again soon!\n" +
-                DIVIDER);
+        System.out.println(DIVIDER
+                + "Nice work today!\n"
+                + "You are one step closer to ultimate fitness!\n"
+                + "See you again soon!\n"
+                + DIVIDER);
     }
 
     public static void printHelpPrompt() {
-        System.out.println(DIVIDER +
-                "Not sure what to do?\n" +
-                "Run command 'help' to see what you can do.\n"
+        System.out.println(DIVIDER
+                + "Not sure what to do?\n"
+                + "Run command 'help' to see what you can do.\n"
                 + DIVIDER);
     }
 }
