699c731e11d53948bef931ba06574b397645b7f4
kwokyto <ryankwok@u.nus.edu>
2021-03-12 11:54:00 +0800

    Fix CheckStyle Errors

--- src/main/java/seedu/fridgefriend/exception/InvalidDateException.java
+++ src/main/java/seedu/fridgefriend/exception/InvalidDateException.java
@@ -4,7 +4,7 @@ package seedu.fridgefriend.exception;
  * Signal that the input format of the date field is incorrect.
  */
 public class InvalidDateException extends Exception {
-    private final static String errorMessage = "Sorry my friend, the date must be in the form 'dd/mm/yy'.\n";
+    private static final String errorMessage = "Sorry my friend, the date must be in the form 'dd/mm/yy'.\n";
 
     public InvalidDateException(String message) {
         super(errorMessage + message);
