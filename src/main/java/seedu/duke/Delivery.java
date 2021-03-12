690a0522414b00e1812dde8b58207fbba5b3178d
JethroPhuah <jethrophuah@gmail.com>
2021-03-12 10:28:36 +0800

    Update Delivery and Item class

--- src/main/java/seedu/duke/Delivery.java
+++ src/main/java/seedu/duke/Delivery.java
@@ -3,10 +3,62 @@ package seedu.duke;
 import java.util.ArrayList;
 
 public class Delivery {
-
+    private String deliveryID;
     private String address; // todo implement enums?
-    private String recipient; // contains name of POP station
+    private String recipient; // contains name of receipient
     private int weight; // weight of the delivery
     private ArrayList<Item> items;
+
+    public Delivery(String deliveryID, String address, String recipient, ArrayList<Item> items) {
+        this.deliveryID = deliveryID;
+        this.address = address;
+        this. recipient = recipient;
+        this.items = items;
+        for (Item item : items) {
+            this.weight += item.getItemWeight();
+        }
+    }
+
+    public String getDeliveryID() {
+        return deliveryID;
+    }
+
+    public void setDeliveryID(String deliveryID) {
+        this.deliveryID = deliveryID;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(ArrayList<Item> items) {
+        this.items = items;
+    }
+
+
 
 }
