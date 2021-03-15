ede9615a54f289ac080c3fff73be2239ebdf3f6a
tzexern <tantzexern@gmail.com>
2021-03-15 13:33:15 +0800

    Fix CI Error for RemoveCommanTest

--- src/test/java/seedu/duke/command/RemoveCommandTest.java
+++ src/test/java/seedu/duke/command/RemoveCommandTest.java
@@ -20,8 +20,6 @@ class RemoveCommandTest {
 
     @Test
     void executeRemoveExpense_removeExpenseCmd_success() {
-        Ui ui = new Ui();
-        Storage storage = new Storage();
         RecordList records = new RecordList();
         records.addRecord(new Expense(new BigDecimal("220.50"), validateDate("2020/01/01"), "electric bills"));
         records.addRecord(new Expense(new BigDecimal("420.50"), validateDate("2020/01/02"), "phone bills"));
@@ -30,25 +28,25 @@ class RemoveCommandTest {
 
         String removeExpenseCmd = "remove -i 1";
         Command command = CommandHandler.parseCommand(ParserHandler.getParseInput(removeExpenseCmd), records);
-        assertTrue(command instanceof RemoveCommand, "Failed test 'executeRemoveExpense_removeExpenseCmd_success', " +
-                "command object returned by parseCommand() is not an instance of RemoveCommand");
+        assertTrue(command instanceof RemoveCommand, "Failed test 'executeRemoveExpense_removeExpenseCmd_success', "
+                + "command object returned by parseCommand() is not an instance of RemoveCommand");
 
-        PrintStream originalOut = System.out;
+        Ui ui = new Ui();
+        Storage storage = new Storage();
         ByteArrayOutputStream removeCmdBos = new ByteArrayOutputStream();
         System.setOut(new PrintStream(removeCmdBos));
-        String expectedOutput = "=========================================================" + System.lineSeparator() +
-                "This record will be removed: [E][2020-01-01] electric bills " + System.lineSeparator() +
-                "=========================================================" + System.lineSeparator();
+        PrintStream originalOut = System.out;
+        String expectedOutput = "=========================================================" + System.lineSeparator()
+                + "This record will be removed: [E][2020-01-01] electric bills " + System.lineSeparator()
+                + "=========================================================" + System.lineSeparator();
         command.execute(records, ui, storage);
-        assertTrue(removeCmdBos.toString().equals(expectedOutput), "Failed test " +
-                "'executeRemoveExpense_removeExpenseCmd_success', wrong output.");
+        assertTrue(removeCmdBos.toString().equals(expectedOutput), "Failed test "
+                + "'executeRemoveExpense_removeExpenseCmd_success', wrong output.");
         System.setOut(originalOut);
     }
 
     @Test
     void executeRemoveLoan_removeLoanCmd_success() {
-        Ui ui = new Ui();
-        Storage storage = new Storage();
         RecordList records = new RecordList();
         records.addRecord(new Expense(new BigDecimal("220.50"), validateDate("2020/01/01"), "electric bills"));
         records.addRecord(new Expense(new BigDecimal("420.50"), validateDate("2020/01/02"), "phone bills"));
@@ -57,25 +55,25 @@ class RemoveCommandTest {
 
         String removeLoanCmd = "remove -i 3";
         Command command = CommandHandler.parseCommand(ParserHandler.getParseInput(removeLoanCmd), records);
-        assertTrue(command instanceof RemoveCommand, "Failed test 'executeRemoveLoan_removeLoanCmd_success', " +
-                "command object returned by parseCommand() is not an instance of RemoveCommand");
+        assertTrue(command instanceof RemoveCommand, "Failed test 'executeRemoveLoan_removeLoanCmd_success', "
+                + "command object returned by parseCommand() is not an instance of RemoveCommand");
 
-        PrintStream originalOut = System.out;
+        Ui ui = new Ui();
+        Storage storage = new Storage();
         ByteArrayOutputStream removeCmdBos = new ByteArrayOutputStream();
         System.setOut(new PrintStream(removeCmdBos));
-        String expectedOutput = "=========================================================" + System.lineSeparator() +
-                "This record will be removed: [L][2020-01-01] loan to bob [ ]" + System.lineSeparator() +
-                "=========================================================" + System.lineSeparator();
+        PrintStream originalOut = System.out;
+        String expectedOutput = "=========================================================" + System.lineSeparator()
+                + "This record will be removed: [L][2020-01-01] loan to bob [ ]" + System.lineSeparator()
+                + "=========================================================" + System.lineSeparator();
         command.execute(records, ui, storage);
-        assertTrue(removeCmdBos.toString().equals(expectedOutput), "Failed test " +
-                "'executeRemoveLoan_removeLoanCmd_success', wrong output.");
+        assertTrue(removeCmdBos.toString().equals(expectedOutput), "Failed test "
+                + "'executeRemoveLoan_removeLoanCmd_success', wrong output.");
         System.setOut(originalOut);
     }
 
     @Test
     void executeRemoveSaving_removeSavingCmd_success() {
-        Ui ui = new Ui();
-        Storage storage = new Storage();
         RecordList records = new RecordList();
         records.addRecord(new Expense(new BigDecimal("220.50"), validateDate("2020/01/01"), "electric bills"));
         records.addRecord(new Expense(new BigDecimal("420.50"), validateDate("2020/01/02"), "phone bills"));
@@ -84,18 +82,20 @@ class RemoveCommandTest {
 
         String removeSavingCmd = "remove -i 4";
         Command command = CommandHandler.parseCommand(ParserHandler.getParseInput(removeSavingCmd), records);
-        assertTrue(command instanceof RemoveCommand, "Failed test 'executeRemoveSaving_removeSavingCmd_success', " +
-                "command object returned by parseCommand() is not an instance of RemoveCommand");
+        assertTrue(command instanceof RemoveCommand, "Failed test 'executeRemoveSaving_removeSavingCmd_success', "
+                + "command object returned by parseCommand() is not an instance of RemoveCommand");
 
-        PrintStream originalOut = System.out;
+        Ui ui = new Ui();
+        Storage storage = new Storage();
         ByteArrayOutputStream removeCmdBos = new ByteArrayOutputStream();
         System.setOut(new PrintStream(removeCmdBos));
-        String expectedOutput = "=========================================================" + System.lineSeparator() +
-                "This record will be removed: [S][2020-01-01] red packet " + System.lineSeparator() +
-                "=========================================================" + System.lineSeparator();
+        PrintStream originalOut = System.out;
+        String expectedOutput = "=========================================================" + System.lineSeparator()
+                + "This record will be removed: [S][2020-01-01] red packet " + System.lineSeparator()
+                + "=========================================================" + System.lineSeparator();
         command.execute(records, ui, storage);
-        assertTrue(removeCmdBos.toString().equals(expectedOutput), "Failed test " +
-                "'executeRemoveSaving_removeSavingCmd_success', wrong output.");
+        assertTrue(removeCmdBos.toString().equals(expectedOutput), "Failed test "
+                + "'executeRemoveSaving_removeSavingCmd_success', wrong output.");
         System.setOut(originalOut);
     }
 }
