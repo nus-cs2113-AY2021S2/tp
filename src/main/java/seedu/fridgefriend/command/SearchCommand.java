7d93aab5ddfe604b494687e589d7aed2162ec94c
SimJJ96 <simjingjie13@gmail.com>
2021-03-15 16:09:45 +0800

    Modify searchCommand display format

--- src/main/java/seedu/fridgefriend/command/SearchCommand.java
+++ src/main/java/seedu/fridgefriend/command/SearchCommand.java
@@ -10,6 +10,7 @@ import seedu.fridgefriend.utilities.Ui;
  */
 public class SearchCommand extends Command {
 
+    public static final int NOT_FOUND = -1;
     private String foodName;
 
     public SearchCommand(String foodName) throws EmptyDescriptionException {
@@ -22,26 +23,39 @@ public class SearchCommand extends Command {
 
     @Override
     public void execute() {
-        showResults(isFound());
+        showResults();
     }
 
-    private boolean isFound() {
-        for (int i = 0; i < Fridge.getSize(); i += 1) {
-            Food food = Fridge.getFood(i);
-            if (food.getFoodName().equals(foodName)) {
-                return true;
-            }
-        }
-        return false;
+    private void showResults() {
+        Ui.printMessage(getMessagePrintedToUser());
     }
 
-    private void showResults(Boolean isFound) {
+    /**
+     * Returns the result of the command.
+     *
+     * @return the message shown to the user
+     */
+    public String getMessagePrintedToUser() {
+        int indexOfFoodStored = getIndexOfFoodStored();
         String message;
-        if (isFound == true) {
-            message = "You have " + foodName + " in your fridge.";
+        if (indexOfFoodStored >= 0) {
+            message = "You have " + foodName + " stored in "
+                    + Fridge.getFood(indexOfFoodStored).getStorageLocation()
+                    + " of your fridge.";
         } else {
             message = "You do not have " + foodName + " in your fridge.";
         }
-        Ui.printMessage(message);
+        return message;
     }
+
+    private int getIndexOfFoodStored() {
+        for (int i = 0; i < Fridge.getSize(); i += 1) {
+            Food food = Fridge.getFood(i);
+            if (food.getFoodName().equals(foodName)) {
+                return i;
+            }
+        }
+        return NOT_FOUND;
+    }
+
 }
