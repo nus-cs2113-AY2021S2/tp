12da1a0625be69c79ba870053c747a277c997e0a
lowwilliam <2595903341@qq.com>
2021-03-16 00:49:13 +0800

    Add BodyWeight class

---
+++ src/main/java/seedu/duke/record/BodyWeight.java
@@ -0,0 +1,30 @@
+package seedu.duke.record;
+
+import java.time.LocalDate;
+
+public class BodyWeight extends Record {
+    private double weight;
+
+    /**
+     * Initializes the object with given record type and date.
+     *
+     * @param type the type of the record.
+     * @param date the date of the record.
+     */
+    public BodyWeight(RecordType type, LocalDate date) {
+        super(type, date);
+    }
+
+    @Override
+    public String getRecordSummary() {
+        return null;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
