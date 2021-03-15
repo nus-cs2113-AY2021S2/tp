36d98dbee9d4f4583d6e4f88651cebe73161b7cb
Leow Xiao Xuan <leowxiaoxuan@gmail.com>
2021-03-15 16:45:43 +0800

    Updated Ui class to include printWelcome and printException

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -12,13 +12,8 @@ public class Duke {
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("Please input a command!");
+        Ui ui = new Ui();
+        ui.printWelcome();
 
         Scanner in = new Scanner(System.in);
         
