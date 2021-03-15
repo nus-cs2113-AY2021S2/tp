ef4a6e71b5144de1ffced60729ebb214560318d4
baggiiiie <dave23eeeeem@gmail.com>
2021-03-16 00:53:56 +0800

    Add Sleep class

---
+++ src/main/java/seedu/duke/record/Sleep.java
@@ -0,0 +1,28 @@
+package seedu.duke.record;
+
+import java.time.LocalDate;
+
+public class Sleep extends Record {
+    private int duration;
+
+    public Sleep(RecordType type, LocalDate date) {
+        super(type, date);
+    }
+
+    public void Sleep(int duration, LocalDate date) {
+
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public String getRecordSummary() {
+        return "You have slept for " + duration + " minutes!";
+    }
+}
