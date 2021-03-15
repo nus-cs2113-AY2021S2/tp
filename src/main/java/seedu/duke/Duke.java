15c1e440a8036133a6b921b5ea01e210efeecdce
lowwilliam <2595903341@qq.com>
2021-03-16 00:49:38 +0800

    Pull merge

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -9,17 +9,9 @@ public class Duke {
         exit();
     }
 
-<<<<<<< HEAD
-        Scanner in = new Scanner(System.in);
-        System.out.println("Hello " + in.nextLine());
-<<<<<<< HEAD
-        
-=======
     private static void exit() {
         UI.printExitMessage();
         System.exit(0);
->>>>>>> a496cf6648346b99d9f86ce88a8ffb3516e64fd4
-=======
->>>>>>> 53df292f2a26aab29619e5f9021146a3ff55f2fa
+        
     }
 }
