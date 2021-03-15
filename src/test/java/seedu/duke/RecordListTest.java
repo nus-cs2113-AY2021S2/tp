5275f6b058511e4731899b6ac5cea75d8012ff91
gerardtwk <gerardtwk@gmail.com>
2021-03-15 16:50:01 +0800

    Refactor junit recordlist test - Extract repeated code used in populating the recordlist across the 3 tests and shift it to getPopulatedRecordList()

--- src/test/java/seedu/duke/RecordListTest.java
+++ src/test/java/seedu/duke/RecordListTest.java
@@ -15,36 +15,52 @@ import static seedu.duke.common.Validators.validateDate;
 public class RecordListTest {
     @Test
     public void addRecord_expenseLoanSavingObjs_success() {
-        RecordList records = new RecordList();
-        records.addRecord(new Expense(new BigDecimal("220.50"), validateDate("2020/01/01"), "electric bills"));
-        records.addRecord(new Loan(new BigDecimal("100"), validateDate("2020/01/01"), "loan to bob"));
-        records.addRecord(new Saving(new BigDecimal("20"), validateDate("2020/01/01"), "red packet"));
-        assertEquals(3, records.getRecordCount());
-        assertEquals("[E][2020-01-01] electric bills ", records.getRecordAt(0).toString());
-        assertEquals("[L][2020-01-01] loan to bob [ ]", records.getRecordAt(1).toString());
-        assertEquals("[S][2020-01-01] red packet ", records.getRecordAt(2).toString());
+        RecordList records = getPopulatedRecordList();
+        assertEquals(3, records.getRecordCount(),
+                "Failed test 'addRecord_expenseLoanSavingObjs_success', expected a record count of 3 but "
+                        + "RecordList::getRecordCount() returned " + records.getRecordCount());
+        assertEquals("[E][2020-01-01] electric bills ", records.getRecordAt(0).toString(),
+                "Failed test 'addRecord_expenseLoanSavingObjs_success', wrong record object at index 0 of "
+                        + "record list");
+        assertEquals("[L][2020-01-01] loan to bob [ ]", records.getRecordAt(1).toString(),
+                "Failed test 'addRecord_expenseLoanSavingObjs_success', wrong record object at index 1 of "
+                        + "record list");
+        assertEquals("[S][2020-01-01] red packet ", records.getRecordAt(2).toString(),
+                "Failed test 'addRecord_expenseLoanSavingObjs_success', wrong record object at index 2 of "
+                        + "record list");
     }
 
     @Test
     public void deleteRecordAt_indexOfAddedRecordToDelete_success() {
-        RecordList records = new RecordList();
-        records.addRecord(new Expense(new BigDecimal("220.50"), validateDate("2020/01/01"), "electric bills"));
-        records.addRecord(new Loan(new BigDecimal("100"), validateDate("2020/01/01"), "loan to bob"));
-        records.addRecord(new Saving(new BigDecimal("20"), validateDate("2020/01/01"), "red packet"));
+        RecordList records = getPopulatedRecordList();
         records.deleteRecordAt(1);
-        assertEquals(2, records.getRecordCount());
-        assertEquals("[E][2020-01-01] electric bills ", records.getRecordAt(0).toString());
-        assertEquals("[S][2020-01-01] red packet ", records.getRecordAt(1).toString());
+        assertEquals(2, records.getRecordCount(),
+                "Failed test 'deleteRecordAt_indexOfAddedRecordToDelete_success', expected a record count of 2 but "
+                        + "RecordList::getRecordCount() returned " + records.getRecordCount());
+        assertEquals("[E][2020-01-01] electric bills ", records.getRecordAt(0).toString(),
+                "Failed test 'deleteRecordAt_indexOfAddedRecordToDelete_success', wrong record object at index 0 of "
+                        + "record list");
+        assertEquals("[S][2020-01-01] red packet ", records.getRecordAt(1).toString(),
+                "Failed test 'deleteRecordAt_indexOfAddedRecordToDelete_success', wrong record object at index 1 of "
+                        + "record list");
     }
 
     @Test
     public void deleteAllRecords_noTestInputs_success() {
+        RecordList records = getPopulatedRecordList();
+        records.deleteAllRecords();
+        assertTrue(records.isEmpty(), "Failed test 'deleteAllRecords_noTestInputs_success', expected an empty "
+                + "record list but RecordList::isEmpty() returned false");
+        assertEquals(0, records.getRecordCount(),
+                "Failed test 'deleteAllRecords_noTestInputs_success', expected a record count of 0 but "
+                        + "RecordList::getRecordCount() returned " + records.getRecordCount());
+    }
+
+    private RecordList getPopulatedRecordList() {
         RecordList records = new RecordList();
         records.addRecord(new Expense(new BigDecimal("220.50"), validateDate("2020/01/01"), "electric bills"));
         records.addRecord(new Loan(new BigDecimal("100"), validateDate("2020/01/01"), "loan to bob"));
         records.addRecord(new Saving(new BigDecimal("20"), validateDate("2020/01/01"), "red packet"));
-        records.deleteAllRecords();
-        assertTrue(records.isEmpty());
-        assertEquals(0, records.getRecordCount());
+        return records;
     }
 }
