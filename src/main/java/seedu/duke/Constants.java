3cb25495be7350853a2103382932c340ebb77869
Emkay16 <mabelkohjx@gmail.com>
2021-03-16 18:20:45 +0800

    Enhance functionality of HelpCommand

--- src/main/java/seedu/duke/Constants.java
+++ src/main/java/seedu/duke/Constants.java
@@ -5,24 +5,25 @@ public class Constants {
             + "Command prefix: add\n"
             + "Argument(s): IC number\n"
             + "Usage: add /<IC_NUMBER>\n"
-            + "Example: add S1234567A\n\n";
+            + "Example: add S1234567A\n";
     public static final String LIST_INFO_MESSAGE = "Show the list of all patients\n"
-            + "Usage: list\n\n";
+            + "Usage: list\n";
     public static final String LOAD_INFO_MESSAGE = "<WIP> Select a specified patient to add and retrieve records\n"
             + "Command prefix: load\n"
             + "Arguments(s): IC number\n"
             + "Usage: load <IC_NUMBER>\n"
-            + "Example: load S1234567A\n\n";
+            + "Example: load S1234567A\n";
     public static final String RECORD_CONSULTATION_INFO_MESSAGE = "Add a consultation record to the selected patient\n"
             + "Command prefix: record\n"
             + "Arguments(s): consultation details\n"
             + "Usage: record <CONSULTATION DETAILS>\n"
-            + "Example: record fever\n\n";
+            + "Example: record fever\n";
     public static final String RETRIEVE_INFO_MESSAGE = "Retrieve past consultation record from the selected patient\n"
-            + "Usage: retrieve\n\n";
+            + "Usage: retrieve\n";
     public static final String EXIT_INFO_MESSAGE = "Exit the program\n"
-            + "Usage: exit\n\n";
+            + "Usage: exit\n";
 
+    public static final String INVALID_COMMAND_MESSAGE = "Invalid command: %s";
     public static final String EXIT_MESSAGE = "Goodbye, we hope to see you again!";
     public static final String WELCOME_MESSAGE = "Welcome to the Patient Manager.\n";
     public static final String INPUT_PROMPT = "Please input a command: ";
