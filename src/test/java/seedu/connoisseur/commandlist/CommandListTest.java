6a9501461ce9472eb68f72af5c2949a6a663585f
Huachen <huachenw24@gmail.com>
2021-03-17 13:08:50 +0800

    add noReviews test

--- src/test/java/seedu/connoisseur/commandlist/CommandListTest.java
+++ src/test/java/seedu/connoisseur/commandlist/CommandListTest.java
@@ -1,34 +1,40 @@
 package seedu.connoisseur.commandlist;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import seedu.connoisseur.ui.Ui;
 import seedu.connoisseur.storage.Storage;
 
-public class CommandListTest {
-    private Ui ui = new Ui(true);
-    private Storage storage = new Storage(ui);
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    @BeforeEach
-    public void setUpStreams() {
+public class CommandListTest {
+    private Ui ui = new Ui(false);
+    private Storage storage = new Storage(ui);
+    private static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @BeforeAll
+    public static void setUpStreams() {
         System.setOut(new PrintStream(outContent));
     }
 
-    @AfterEach
-    public void cleanUpStreams() {
-        System.setOut(null);
+    @AfterAll
+    public static void cleanUpStreams() {
+        System.setOut(System.out);
+    }
+
+    @Test
+    public void sanityCheck() {
+        System.out.print("hello");
+        assertEquals("hello", outContent.toString());
     }
 
     @Test
     public void listReviews_noReviews_noReviewsFound() {
         new CommandList(ui, storage).listReviews("ANY_SORT_METHOD");
-        assertEquals("No reviews found. :(", outContent.toString());
+        assertEquals("No reviews found. :(\r\n", outContent.toString());
     }
 }
