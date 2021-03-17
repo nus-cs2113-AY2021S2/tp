15b0f741a46739171b998d4742dcb9cbbd65caab
Huachen <60135779+huachen24@users.noreply.github.com>
2021-03-18 00:09:12 +0800

    fix checkstyle

--- src/main/java/seedu/connoisseur/exceptions/ConnoisseurException.java
+++ src/main/java/seedu/connoisseur/exceptions/ConnoisseurException.java
@@ -1,14 +1,14 @@
 package seedu.connoisseur.exceptions;
 
-public class ConnoisseurException extends Exception{
+public class ConnoisseurException extends Exception {
 
     /**
      * Creates DukeExceptions for methods to throw.
      * @param errorMessage Error message.
      */
 
-    public ConnoisseurException(String errorMessage){
-        String WARNING_DIVIDER = "\txxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n";
+    public ConnoisseurException(String errorMessage) {
+        private static String WARNING_DIVIDER = "\txxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n";
         System.out.print(WARNING_DIVIDER + "\tERROR: " + errorMessage + WARNING_DIVIDER);
     }
 }
