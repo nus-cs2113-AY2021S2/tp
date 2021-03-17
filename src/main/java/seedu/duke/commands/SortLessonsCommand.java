e1e0bc21132e611bd90b026d648e43dcf3fcf1b9
Hemrish Bundhoo <e0407840@u.nus.edu>
2021-03-17 17:46:41 +0800

    Add Java-Doc for SortLessonsCommand

--- src/main/java/seedu/duke/commands/SortLessonsCommand.java
+++ src/main/java/seedu/duke/commands/SortLessonsCommand.java
@@ -12,7 +12,9 @@ import java.util.ArrayList;
 import static seedu.duke.commands.ListLessonsCommand.printLessons;
 import static seedu.duke.common.Messages.MESSAGE_SORT_LESSON_LIST;
 
-
+/**
+ * Represents the command used to sort the lessons on the list of lessons.
+ */
 public class SortLessonsCommand extends Command {
 
     @Override
@@ -23,16 +25,30 @@ public class SortLessonsCommand extends Command {
         sortLessonList(ui, lessonList);
     }
 
+    /**
+     * Sorts lessons based on their lesson type.
+     *
+     * @param ui         Instance of UI.
+     * @param lessonList ArrayList of lessons.
+     */
     private void sortLessonList(UI ui, ArrayList<Lesson> lessonList) {
         ArrayList<Lesson> sortedLessonList = new ArrayList<>();
-        sortByLessonType(lessonList, sortedLessonList, LessonType.LECTURE);
-        sortByLessonType(lessonList, sortedLessonList, LessonType.TUTORIAL);
-        sortByLessonType(lessonList, sortedLessonList, LessonType.LAB);
+        filterByLessonType(lessonList, sortedLessonList, LessonType.LECTURE);
+        filterByLessonType(lessonList, sortedLessonList, LessonType.TUTORIAL);
+        filterByLessonType(lessonList, sortedLessonList, LessonType.LAB);
         lessonList = sortedLessonList;
         printLessons(lessonList, ui);
     }
 
-    public void sortByLessonType(ArrayList<Lesson> lessonList, ArrayList<Lesson> sortedLessonList, LessonType lecture) {
+    /**
+     * Filters lessons of a particular type and add them to a new list.
+     *
+     * @param lessonList       ArrayList of lessons.
+     * @param sortedLessonList Sorted arrayList of lessons.
+     * @param lecture          Type of Lecture
+     */
+    public void filterByLessonType(ArrayList<Lesson> lessonList, ArrayList<Lesson> sortedLessonList,
+                                   LessonType lecture) {
         for (Lesson lesson : lessonList) {
             if (lesson.getLessonType().equals(lecture)) {
                 sortedLessonList.add(lesson);
