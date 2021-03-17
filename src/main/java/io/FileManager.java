e2d421ac53cfe970ad64fc41db0dd3070fd34932
cloudy3 <cjingfeng98@gmail.com>
2021-03-18 01:49:24 +0800

    Add File Manager

---
+++ src/main/java/io/FileManager.java
@@ -0,0 +1,57 @@
+package io;
+
+import parser.DataParser;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FileManager {
+
+    private static DataParser dataParser;
+
+    public FileManager() {
+        dataParser = new DataParser();
+    }
+
+    // TODO: insert object
+    public void saveFile(ArrayList<_> _) throws IOException {
+        File path = new File("_.txt");
+        if (!path.exists()) {
+            if (!path.createNewFile()) {
+                throw new IOException();
+            }
+        }
+        FileWriter fileWriter = new FileWriter(path);
+        for (_ _ : _) {
+            fileWriter.write(_.formatData());
+        }
+        fileWriter.flush();
+        fileWriter.close();
+    }
+
+    public ArrayList<_> loadFile() throws FileNotFoundException {
+        ArrayList<_> _ = new ArrayList<>();
+
+        File path = new File("_.txt");
+        if (!path.exists()) {
+            throw new FileNotFoundException();
+        }
+        Scanner scanner = new Scanner(path);
+        try {
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                _ _ = dataParser.parseData(line);
+                if (_ != null) {
+                    _.add(_);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Failed to load!");
+        }
+        return _;
+    }
+}
