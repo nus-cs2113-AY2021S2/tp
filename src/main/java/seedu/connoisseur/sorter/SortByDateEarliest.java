e552bfa62389a28fd3a468579b84f38fb7c60e26
Krithigha24 <krithigha242000@gmail.com>
2021-03-18 11:16:55 +0800

    Deleted sort by stars & added Time to reviews

--- src/main/java/seedu/connoisseur/sorter/SortByDateEarliest.java
+++ src/main/java/seedu/connoisseur/sorter/SortByDateEarliest.java
@@ -16,8 +16,8 @@ public class SortByDateEarliest implements Comparator<Review> {
      */
     public int compare(Review a, Review b) {
         try {
-            Date dateA = new SimpleDateFormat("MMM d yyyy").parse(a.getDate());
-            Date dateB = new SimpleDateFormat("MMM d yyyy").parse(b.getDate());
+            Date dateA = new SimpleDateFormat("MMM d yyyy HH:mm:ss a").parse(a.getDateTime());
+            Date dateB = new SimpleDateFormat("MMM d yyyy HH:mm:ss a").parse(b.getDateTime());
             return dateA.compareTo(dateB);
         } catch (ParseException e) {
             System.out.println("Wrong date format");
