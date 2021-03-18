605e38eb28452478d32f24efc169f63128367219
tzexern <tantzexern@gmail.com>
2021-03-19 04:56:08 +0800

    Update CreditScoreCommand

--- src/main/java/seedu/duke/command/CreditScoreCommand.java
+++ src/main/java/seedu/duke/command/CreditScoreCommand.java
@@ -1,5 +1,6 @@
 package seedu.duke.command;
 
+import seedu.duke.common.ArgumentType;
 import seedu.duke.exception.CommandException;
 import seedu.duke.record.RecordList;
 import seedu.duke.storage.Storage;
@@ -7,16 +8,42 @@ import seedu.duke.ui.Ui;
 
 import java.util.ArrayList;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
+import static seedu.duke.command.Utils.validateArguments;
+
+/**
+ * Handles all operations related to the credit score command.
+ */
 public class CreditScoreCommand extends Command {
 
     protected static final String COMMAND_CREDIT_SCORE = "creditscore";
+    private static final ArgumentType[] ARGUMENT_TYPE_ORDER = {
+            ArgumentType.COMMAND,
+            ArgumentType.VALUE
+    };
 
+    /**
+     * Constructor to validate the format for credit score command.
+     *
+     * @param arguments parsed input containing arguments.
+     * @throws CommandException contains the error messages when a incorrect format is detected.
+     */
     public CreditScoreCommand(ArrayList<String> arguments) throws CommandException {
+        validateArguments(arguments, ARGUMENT_TYPE_ORDER, COMMAND_CREDIT_SCORE);
     }
 
-    private int getDayDifference(LocalDate issueDate, LocalDate returnDate) {
-        return 0;
+    private long getDayDifference(LocalDate issueDate, LocalDate returnDate) {
+        LocalDate from = issueDate;
+        LocalDate to;
+        if(returnDate == null) {
+            to = LocalDate.now();
+        } else {
+            assert returnDate != null : "returnDate should not be empty";
+            to = returnDate;
+        }
+        long dayDifference = ChronoUnit.DAYS.between(from, to);
+        return dayDifference;
     }
 
     private int getCreditScore(int days, int score) {
