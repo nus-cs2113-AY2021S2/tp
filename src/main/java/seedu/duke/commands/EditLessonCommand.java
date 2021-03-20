709ec08f57928001bf3b67785a80823ebb1c3274
ivanchongzhien <icze2009@gmail.com>
2021-03-20 16:04:24 +0800

    Add skeleton for editLessonCommand

---
+++ src/main/java/seedu/duke/commands/EditLessonCommand.java
@@ -0,0 +1,19 @@
+package seedu.duke.commands;
+
+import seedu.duke.lesson.Lesson;
+import seedu.duke.module.Module;
+import seedu.duke.module.ModuleList;
+import seedu.duke.ui.UI;
+
+import java.util.ArrayList;
+
+public class EditLessonCommand extends Command {
+    @Override
+    public void execute(UI ui) {
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
+}
