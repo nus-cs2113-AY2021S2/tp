4f6e85911fc2675f8e1f1bc4d71d43151243e4da
Lee Yang Peng <lee.yangpeng@dhs.sg>
2021-03-21 18:14:48 +0800

    Improved JUnit Test

--- src/test/java/seedu/fridgefriend/command/AddCommandTest.java
+++ src/test/java/seedu/fridgefriend/command/AddCommandTest.java
@@ -21,7 +21,7 @@ class AddCommandTest {
     }
 
     @Test
-    public void addCommand_foodInCorrectFormat_successfullyAdded() throws InvalidDateException {
+    public void addCommand_foodCorrectFormat_successfullyAdded() throws InvalidDateException {
         AddCommand addCommand = new AddCommand("chicken", FoodCategory.MEAT,
                 "30-06-2021", FoodStorageLocation.FREEZER);
         addCommand.setData(fridge);
@@ -38,6 +38,34 @@ class AddCommandTest {
                 + "expiry: 30-06-2021, stored in: FREEZER";
         String actualMessage = addCommand.getMessagePrintedToUser();
         assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    public void addCommand_foodCorrectFormat_changeFoodParameters() throws InvalidDateException {
+        AddCommand addCommand = new AddCommand("chicken", FoodCategory.MEAT,
+                "30-06-2021", FoodStorageLocation.FREEZER);
+        addCommand.setData(fridge);
+        addCommand.execute();
+        assertEquals("chicken", fridge.getFood(0).getFoodName());
+        assertEquals(FoodCategory.MEAT, fridge.getFood(0).getCategory());
+        assertEquals(FoodStorageLocation.FREEZER, fridge.getFood(0).getStorageLocation());
+
+        ExpiryDate expiryDate = new ExpiryDate("30-06-2021");
+        assertEquals(expiryDate.getExpiry(), fridge.getFood(0).getExpiryDate().getExpiry());
+
+        String expectedMessage = "Great! I have added chicken into your fridge.\n"
+                + "Details: Food name: chicken, category: MEAT, "
+                + "expiry: 30-06-2021, stored in: FREEZER";
+        String actualMessage = addCommand.getMessagePrintedToUser();
+        assertEquals(expectedMessage, actualMessage);
+
+
+        fridge.getFood(0).setExpiryDate("12-04-2021");
+        assertEquals("12-04-2021", fridge.getFood(0).getExpiryDate().toString());
+        fridge.getFood(0).setFoodName("cooked chicken");
+        fridge.getFood(0).setCategory(FoodCategory.READY_TO_EAT);
+        assertEquals(FoodCategory.READY_TO_EAT, fridge.getFood(0).getCategory());
+        assertEquals("cooked chicken", fridge.getFood(0).getFoodName());
     }
 
     @Test
