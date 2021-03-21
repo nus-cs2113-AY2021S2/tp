bed97da7b34583205a0de3eb15b36fd4689fcc3f
LAPTOP-VRGPN0FU\MingS <e0425961@u.nus.edu>
2021-03-21 14:25:56 +0800

    v1.0 added JunitTest

---
+++ src/test/java/seedu/doctorappointments/DoctorAppointmentParserTest.java
@@ -0,0 +1,26 @@
+package seedu.doctorappointments;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DoctorAppointmentParserTest {
+    private Parser parser;
+    private AppointmentActions actions;
+
+    @Test
+    public void returnCommand() throws Exception {
+        boolean output = Parser.parse("return");
+        assertTrue(output);
+    }
+
+    @Test
+    public void unknownCommand() throws Exception {
+        boolean output = Parser.parse("unknown");
+        assertFalse(output);
+    }
+
+}
