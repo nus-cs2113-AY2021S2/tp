a35aae3ce74cdc90262300b1af7a21c0903516a6
SimJJ96 <simjingjie13@gmail.com>
2021-03-22 23:25:55 +0800

    Update Commands to allow add quantity

--- src/main/java/seedu/fridgefriend/command/AddCommand.java
+++ src/main/java/seedu/fridgefriend/command/AddCommand.java
@@ -1,23 +1,26 @@
 package seedu.fridgefriend.command;
 
 import seedu.fridgefriend.exception.InvalidDateException;
+import seedu.fridgefriend.exception.InvalidQuantityException;
 import seedu.fridgefriend.food.Food;
 import seedu.fridgefriend.food.FoodCategory;
 import seedu.fridgefriend.food.FoodStorageLocation;
+import seedu.fridgefriend.food.Quantity;
 import seedu.fridgefriend.utilities.Ui;
 
 /**
  * Represents a command to add a food item into the Fridge.
- * When calling constructor, foodName, category, expiryDate, and location are necessary fields
+ * When calling constructor, foodName, category, expiryDate, location
+ * and quantity are necessary fields
  */
 public class AddCommand extends Command {
 
     private final Food foodToAdd;
 
     public AddCommand(String foodName, FoodCategory category, String expiryString,
-            FoodStorageLocation location) throws InvalidDateException {
-        this.foodToAdd = new Food(category, foodName, expiryString, location);
-
+                      FoodStorageLocation location, Quantity quantity)
+            throws InvalidDateException, InvalidQuantityException {
+        this.foodToAdd = new Food(category, foodName, expiryString, location, quantity);
     }
 
     @Override
--- src/main/java/seedu/fridgefriend/command/HelpCommand.java
+++ src/main/java/seedu/fridgefriend/command/HelpCommand.java
@@ -12,7 +12,8 @@ import seedu.fridgefriend.utilities.Ui;
 public class HelpCommand extends Command {
 
     private static final String COMMANDS = "These are the list of available commands:\n"
-            + "\tadd foodName /cat categoryName /exp dd-mm-yyyy /loc storageLocation\n"
+            + "\tadd foodName /cat categoryName /exp dd-mm-yyyy /loc storageLocation /qty foodQuantity\n"
+            + "\tadd foodName /cat categoryName /exp dd-mm-yyyy /loc storageLocation /wgt foodWeight\n"
             + "\tlist\n"
             + "\tlist categoryName\n"
             + "\tremove index\n"
