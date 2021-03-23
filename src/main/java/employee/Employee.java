ad2e7ce7a594b470d897f683c5398414d0170cd7
lihaoyangML <60748402+lihaoyangML@users.noreply.github.com>
2021-03-23 18:15:20 +0800

    add constraints on schedule

--- src/main/java/employee/Employee.java
+++ src/main/java/employee/Employee.java
@@ -5,6 +5,12 @@ import java.util.ArrayList;
 public class Employee {
     private String name;
     private ArrayList<String> schedules = new ArrayList<>();
+    private static final int MAX_DAY_OF_MONTH = 31;
+    private static final int MIN_DAY_OF_MONTH = 1;
+    private static final int MAX_MONTH_OF_YEAR = 12;
+    private static final int MIN_MONTH_OF_YEAR = 1;
+    private static final int MAX_ALLOWABLE_YEAR = 2099;
+    private static final int MIN_ALLOWABLE_YEAR = 2021;
 
     public Employee(String name){
         this.name = name;
@@ -19,7 +25,41 @@ public class Employee {
     }
 
     public void addSchedule(String schedule){
-        schedules.add(schedule);
+        if(isScheduleValid(schedule) == true){
+            schedules.add(schedule);
+            System.out.println("schedule added");
+        }else {
+            System.out.println("Please enter a valid schedule in this format: dd/mm/yyyy");
+        }
+    }
+
+    private boolean isScheduleValid(String schedule){
+        if(schedule.length()!= 10){
+            return false;
+        }
+        if(schedule.substring("dd".length(), "dd/".length()).equals("/") == false ||
+        schedule.substring("dd/mm".length(), "dd/mm/".length()).equals("/") == false){
+            return false;
+        }
+        String day = schedule.substring(0,"dd".length());
+        String month = schedule.substring("dd/".length(), "dd/mm".length());
+        String year = schedule.substring("dd/mm/".length(), "dd/mm/yyyy".length());
+        if(isInteger(day) == false || isInteger(month) == false || isInteger(year) == false){
+            return false;
+        }
+        int yyyy = Integer.parseInt(year);
+        int mm = Integer.parseInt(month);
+        int dd = Integer.parseInt(day);
+        if(yyyy<MIN_ALLOWABLE_YEAR || yyyy>MAX_ALLOWABLE_YEAR){
+            return false;
+        }
+        if(mm<MIN_MONTH_OF_YEAR || mm>MAX_MONTH_OF_YEAR){
+            return false;
+        }
+        if(dd<MIN_DAY_OF_MONTH || dd>MAX_DAY_OF_MONTH){
+            return false;
+        }
+        return true;
     }
 
     public void dropSchedule(String schedule){
@@ -38,5 +78,16 @@ public class Employee {
             formattedString.append(schedule);
         }
         return formattedString +"\n";
+    }
+
+    public static boolean isInteger(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch(NumberFormatException e) {
+            return false;
+        } catch(NullPointerException e) {
+            return false;
+        }
+        return true;
     }
 }
