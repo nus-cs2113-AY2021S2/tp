9910f9a1c19fc2eb7a9b16d96c8be230c913553c
angjiahui <angjiahui@users.noreply.github.com>
2021-03-23 12:45:57 +0800

    Add junit for list reviews, no reviews exist

--- src/test/java/seedu/connoisseur/commandlist/CommandListTest.java
+++ src/test/java/seedu/connoisseur/commandlist/CommandListTest.java
@@ -1,2 +1,23 @@
-package seedu.connoisseur.commandlist;public class CommandListTest {
+package seedu.connoisseur.commandlist;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.*;
+
+public class CommandListTest {
+
+    @Test
+     void listReview_noReviewsExist() {
+
+        PrintStream save_out=System.out;
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        System.out.println("You have no reviews, type 'new' to start!");
+        assertEquals("You have no reviews, type 'new' to start!\n", out.toString());
+
+        System.setOut(save_out);
+    }
 }
