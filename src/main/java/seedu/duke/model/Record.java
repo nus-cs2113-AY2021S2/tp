033425a73abbed75b8ee43d8fbdec380acc4f8b5
Brandon Foong <brandonfoongyankai@gmail.com>
2021-03-23 22:07:13 +0800

    Update Record class to maintain compatibility with the Patient class

--- src/main/java/seedu/duke/model/Record.java
+++ src/main/java/seedu/duke/model/Record.java
@@ -1,33 +1,53 @@
 package seedu.duke.model;
 
+import java.util.ArrayList;
+
 /**
  * Each instance of this class represent a visit record. It can contain
  * medical diagnosis, prescription, test results, etc.
- * TODO: For v1.0 iteration, we will just implement a single string field to store everything
  */
 public class Record {
-    protected String consultationDetail;
+    protected ArrayList<String> symptoms, diagnoses, prescriptions;
 
     /**
      * Create a new visit record with details as a single string.
-     *
-     * @param consultationDetail detail of the visit record (diagnosis, prescription, etc.)
      */
-    public Record(String consultationDetail) {
-        this.consultationDetail = consultationDetail;
+    public Record() {
+        this.symptoms = new ArrayList<>();
+        this.diagnoses = new ArrayList<>();
+        this.prescriptions = new ArrayList<>();
     }
 
     /**
      * Obtain the consultation detail of this record.
      *
-     * @return Consultation detail related to this record
+     * @return A String containing the consultation details contained in this record
      */
     public String getConsultationDetail() {
+        String consultationDetail = "";
+        consultationDetail += "Symptoms:\n";
+        for (String symptom : symptoms) {
+            consultationDetail += "\t" + symptom + "\n";
+        }
+        consultationDetail += "Diagnoses:\n";
+        for (String diagnosis : diagnoses) {
+            consultationDetail += "\t" + diagnosis + "\n";
+        }
+        consultationDetail += "Prescriptions:\n";
+        for (String prescription : prescriptions) {
+            consultationDetail += "\t" + prescription + "\n";
+        }
         return consultationDetail;
     }
 
     @Override
     public String toString() {
-        return consultationDetail;
+        return getConsultationDetail();
+    }
+
+    public void addDetails(String symptom, String diagnosis, String prescription) {
+        symptoms.add(symptom);
+        diagnoses.add(diagnosis);
+        prescriptions.add(prescription);
     }
 }
