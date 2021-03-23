a5bdd122898bb1ce6fb58843c3f73a0c8fee7626
Krithigha24 <krithigha242000@gmail.com>
2021-03-23 23:51:22 +0800

    Added J-unit test for checkAndPrintDuplicate() method in CommandList

--- src/test/java/seedu/connoisseur/commandlist/CommandListTest.java
+++ src/test/java/seedu/connoisseur/commandlist/CommandListTest.java
@@ -3,11 +3,16 @@ package seedu.connoisseur.commandlist;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.After;
+import seedu.connoisseur.review.Review;
+import seedu.connoisseur.storage.Storage;
+import seedu.connoisseur.ui.Ui;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.io.PrintStream;
 import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
 
 public class CommandListTest {
 
@@ -31,5 +36,12 @@ public class CommandListTest {
         System.out.print("You have no reviews, type 'new' to start!");
         assertEquals("You have no reviews, type 'new' to start!", outContent.toString());
         restoreStreams();
+    }
+
+    @Test
+    void checkAndPrintDuplicate() {
+        CommandList commandList = new CommandList(new ArrayList<String>() , new Ui() ,new Storage(new Ui()));
+        commandList.reviewList = new ArrayList<Review>();
+        assertFalse(commandList.checkAndPrintDuplicate("Avengers"));
     }
 }
