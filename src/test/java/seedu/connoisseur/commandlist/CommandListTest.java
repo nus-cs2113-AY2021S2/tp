8ea86bad8e125592d37784941c5f2cb5199b75e7
Krithigha24 <krithigha242000@gmail.com>
2021-03-23 23:57:49 +0800

    fixed checkstyle error

--- src/test/java/seedu/connoisseur/commandlist/CommandListTest.java
+++ src/test/java/seedu/connoisseur/commandlist/CommandListTest.java
@@ -40,7 +40,7 @@ public class CommandListTest {
 
     @Test
     void checkAndPrintDuplicate() {
-        CommandList commandList = new CommandList(new ArrayList<String>() , new Ui() ,new Storage(new Ui()));
+        CommandList commandList = new CommandList(new ArrayList<String>(), new Ui(), new Storage(new Ui()));
         commandList.reviewList = new ArrayList<Review>();
         assertFalse(commandList.checkAndPrintDuplicate("Avengers"));
     }
