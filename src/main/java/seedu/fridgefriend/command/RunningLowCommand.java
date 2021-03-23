ed8e6f900a3c342adba293b3b0cbd5e805bf11d8
kwokyto <ryankwok@u.nus.edu>
2021-03-24 01:23:38 +0800

    Add stockedUpMessage

--- src/main/java/seedu/fridgefriend/command/RunningLowCommand.java
+++ src/main/java/seedu/fridgefriend/command/RunningLowCommand.java
@@ -6,6 +6,8 @@ import seedu.fridgefriend.utilities.Ui;
 public class RunningLowCommand extends Command {
 
     private String message = "You are running low on food in these categories:";
+    private String stockedUpMessage = "Congrats! You are all stocked up on food! :D";
+    private boolean isStockedUp = true;
     private int index = 1;
     
     public RunningLowCommand() {
@@ -17,11 +19,15 @@ public class RunningLowCommand extends Command {
         for (FoodCategory foodCategory : FoodCategory.values()) {
             updateMessage(foodCategory);
         }
+        if (isStockedUp) {
+            message = stockedUpMessage;
+        }
         Ui.printMessage(message);
     }
 
     private void updateMessage(FoodCategory foodCategory) {
         if (fridge.isRunningOut(foodCategory)) {
+            isStockedUp = false;
             int totalQuantity = fridge.getTotalQuantity(foodCategory);
             String entry = "\n" + index + ". " + foodCategory.toString() + " quantity: " + totalQuantity
                     + " out of " + FoodCategory.getMinimumQuantity(foodCategory);
