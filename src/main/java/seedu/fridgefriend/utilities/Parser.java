5250d0c35e3fdef6f65f96606e0f9792b9312319
kwokyto <ryankwok@u.nus.edu>
2021-03-24 15:33:09 +0800

    Revert "Removed ClearCommand from User"
    This reverts commit 5a964aba1c728c6135f46022185bad6bcd693224.

--- src/main/java/seedu/fridgefriend/utilities/Parser.java
+++ src/main/java/seedu/fridgefriend/utilities/Parser.java
@@ -8,6 +8,7 @@ import java.util.regex.Pattern;
 
 import seedu.fridgefriend.command.AddCommand;
 import seedu.fridgefriend.command.ByeCommand;
+import seedu.fridgefriend.command.ClearCommand;
 import seedu.fridgefriend.command.Command;
 import seedu.fridgefriend.command.ExpiringCommand;
 import seedu.fridgefriend.command.HelpCommand;
@@ -117,6 +118,9 @@ public class Parser {
             break;
         case "help":
             command = Parser.getHelpCommand();
+            break;
+        case "clear":
+            command = Parser.getClearCommand();
             break;
         case "bye":
             command = Parser.getByeCommand();
@@ -313,6 +317,16 @@ public class Parser {
     public static Command getHelpCommand() {
         Command helpCommand = new HelpCommand();
         return helpCommand;
+    }
+
+    /**
+     * Returns a ClearCommand object.
+     *
+     * @return ClearCommand object
+     */
+    public static Command getClearCommand() {
+        Command clearCommand = new ClearCommand();
+        return clearCommand;
     }
 
     /**
