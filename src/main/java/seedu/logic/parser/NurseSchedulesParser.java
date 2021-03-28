86f664d75ef94704268a917853d301c97f1bc938
AlexanderTanJunAn <e0406248@u.nus.edu>
2021-03-28 12:44:46 +0800

    Post consultation edits

--- src/main/java/seedu/logic/parser/NurseSchedulesParser.java
+++ src/main/java/seedu/logic/parser/NurseSchedulesParser.java
@@ -1,10 +1,19 @@
 package seedu.logic.parser;
 
+import seedu.exceptions.nurseschedules.EmptyListException;
+import seedu.exceptions.nurseschedules.NurseIdNotFound;
 import seedu.exceptions.nurseschedules.WrongInputsException;
+import seedu.exceptions.staffexceptions.AbortException;
+import seedu.logic.command.NurseScheduleActions;
+import seedu.model.object.NurseSchedule;
+import seedu.storage.NurseScheduleStorage;
+import seedu.ui.NurseScheduleUI;
+import seedu.ui.UI;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
 
 public class NurseSchedulesParser {
@@ -25,7 +34,7 @@ public class NurseSchedulesParser {
      * @param text User input
      * @return First word of user input
      */
-    public static String getFirstWord(String text) {
+    public String getFirstWord(String text) {
         int index = text.indexOf(' ');
 
         if (index > -1) {
@@ -38,7 +47,7 @@ public class NurseSchedulesParser {
         }
     }
 
-    public static String[] getDetails(String text) throws WrongInputsException {
+    public String[] getDetails(String text) throws WrongInputsException {
         String[] details = new String[3];
 
         String[] parts = text.split(" ", 0);
@@ -66,5 +75,55 @@ public class NurseSchedulesParser {
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
 
         return formatter.format(date);
+    }
+
+    public boolean commandHandler(List<NurseSchedule> nurseSchedules, String command, String line) {
+        NurseScheduleActions actions = new NurseScheduleActions();
+        NurseScheduleStorage storage = new NurseScheduleStorage();
+        NurseSchedulesParser parser = new NurseSchedulesParser();
+
+        switch (command) {
+        case "add":
+            try {
+                actions.addSchedule(nurseSchedules, parser.getDetails(line));
+                storage.writeToFile(nurseSchedules);
+            } catch (WrongInputsException e) {
+                System.out.println(e.getMessage());
+                NurseScheduleUI.addHelpMessage();
+            }
+            break;
+        case "list":
+            try {
+                actions.listSchedules(nurseSchedules, parser.getDetails(line));
+            } catch (WrongInputsException e) {
+                NurseScheduleUI.invalidInputsMessage();
+                NurseScheduleUI.listHelpMessage();
+            } catch (EmptyListException e) {
+                System.out.println(e.getMessage());
+            } catch (NurseIdNotFound e) {
+                System.out.println(e.getMessage());
+            }
+            break;
+        case "delete":
+            try {
+                actions.deleteSchedule(nurseSchedules, parser.getDetails(line));
+                storage.writeToFile(nurseSchedules);
+            } catch (WrongInputsException e) {
+                System.out.println(e.getMessage());
+                NurseScheduleUI.deleteHelpMessage();
+            }
+            break;
+        case "help":
+            NurseScheduleUI.printNurseScheduleHelpList();
+            break;
+        case "return":
+            storage.writeToFile(nurseSchedules);
+            NurseScheduleUI.returningToStartMenuMessage();
+            return false;
+        default:
+            NurseScheduleUI.invalidCommandMessage();
+            break;
+        }
+        return true;
     }
 }
