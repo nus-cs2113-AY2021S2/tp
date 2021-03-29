10cfe8fa9a4392757234cc994ade155578a70b60
LAPTOP-VRGPN0FU\MingS <e0425961@u.nus.edu>
2021-03-29 16:09:01 +0800

    AppointmentParser Change delimeter to /

--- src/main/java/seedu/logic/parser/DoctorAppointmentParser.java
+++ src/main/java/seedu/logic/parser/DoctorAppointmentParser.java
@@ -10,25 +10,18 @@ import seedu.ui.UI;
 public class DoctorAppointmentParser {
 
     public static Command parse(String input, AppointmentActions details) throws Exception {
-        String[] inputArray = input.split(" ");
+        String[] inputArray = input.split("/");
         Command c = null;
-        System.out.println("IN parser");
 
         switch (inputArray[0]) {
         case "add":
             c = new DoctorAppointmentAdd(inputArray);
-            System.out.println("IN ADD");
-
             break;
         case "list":
             c = new DoctorAppointmentList(inputArray);
-            System.out.println("IN LIST");
-
             break;
         case "delete":
             c = new DoctorAppointmentDelete(inputArray);
-            System.out.println("IN DELETE");
-
             break;
         case "return":
             c = new DoctorAppointmentReturn();
