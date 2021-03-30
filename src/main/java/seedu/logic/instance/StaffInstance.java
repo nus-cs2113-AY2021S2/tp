e10b67e8981eeae0fe21e0699c5c0c33c83d2c16
blank-bank <yipowen@yahoo.com.sg>
2021-03-30 15:49:21 +0800

    Standardise Constant Output

--- src/main/java/seedu/logic/instance/StaffInstance.java
+++ src/main/java/seedu/logic/instance/StaffInstance.java
@@ -10,7 +10,6 @@ import seedu.ui.StaffUI;
 import seedu.ui.UI;
 
 import java.io.IOException;
-import java.util.Scanner;
 
 public class StaffInstance {
     private StaffUI staffUI;
@@ -35,11 +34,9 @@ public class StaffInstance {
             StaffUI.corruptedFileErrorMessage();;
         }
         StaffUI.staffMenuHeader();
-        Scanner in = new Scanner(System.in);
         while (true) {
-            StaffUI.staffMenuPrompt();
             String line;
-            line = in.nextLine();
+            line = staffUI.getInput("Staff");
             try {
                 Command c = staffParser.commandHandler(line);
                 if (c==null){
