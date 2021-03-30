4638ac68febd7e94f8a9fb150f4c98eb075df50e
wjchoi0712 <e0376605@u.nus.edu>
2021-03-30 20:02:36 +0800

    fixed checkstyle

--- src/test/java/seedu/duke/RouterTest.java
+++ src/test/java/seedu/duke/RouterTest.java
@@ -14,17 +14,15 @@ class RouterTest {
     @Test
     void printShortestDistance_linkedBlocks_expectRoute() throws InvalidBlockException {
         NusMap nusMap = new NusMap();
-        BlockAlias blockAlias = new BlockAlias();
-        String out = new Router().execute(nusMap, blockAlias, "E2","E7");
+        String out = new Router().execute(nusMap, "E2","E7");
         assertEquals(out, "Route: E2 -> E3 -> E4 -> E4A -> EW2 -> E6 -> E7");
     }
 
     @Test
     void printShortestDistance_unknownBlock_expectException() {
         NusMap nusMap = new NusMap();
-        BlockAlias blockAlias = new BlockAlias();
         assertThrows(InvalidBlockException.class, () -> {
-            new Router().execute(nusMap, blockAlias, "AS2","E7");
+            new Router().execute(nusMap, "AS2","E7");
         });
     }
 
@@ -32,10 +30,10 @@ class RouterTest {
     void printShortestDistance_linkedBlocks_expectRouteAfterRepeatedCall() throws InvalidBlockException {
         NusMap nusMap = new NusMap();
         BlockAlias blockAlias = new BlockAlias();
-        String out1 = new Router().execute(nusMap, blockAlias, "E2","E7");
+        String out1 = new Router().execute(nusMap, "E2","E7");
         assertEquals(out1, "Route: E2 -> E3 -> E4 -> E4A -> EW2 -> E6 -> E7");
         nusMap.resetVisitedFlag();
-        String out2 = new Router().execute(nusMap, blockAlias, "E2", "E7");
+        String out2 = new Router().execute(nusMap, "E2", "E7");
         assertEquals(out2, "Route: E2 -> E3 -> E4 -> E4A -> EW2 -> E6 -> E7");
     }
 }
