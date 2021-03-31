630ed8d15892e1558bdfc87cba1072b71031c5da
LAPTOP-VRGPN0FU\MingS <e0425961@u.nus.edu>
2021-03-31 20:55:44 +0800

    Updated Commands

--- src/main/java/seedu/ui/DoctorAppointmentUI.java
+++ src/main/java/seedu/ui/DoctorAppointmentUI.java
@@ -4,19 +4,26 @@ import seedu.model.DoctorAppointment;
 
 import java.text.ParseException;
 
+import static seedu.duke.Constants.*;
+import static seedu.duke.Constants.MARK_BLANK;
+
 public class DoctorAppointmentUI extends UI {
     public static void doctorAppointmentsWelcome() {
         System.out.println("Welcome to the Appointments' Menu!");
     }
 
     public static void doctorAppointmentHelp() {
-        System.out.println("Welcome to the Appointments Commands section!");
-        System.out.println("Here is a list of doctor appointments commands: ");
-        System.out.println("\"add [Doctor ID] [Patient's Name] [Gender] [DDMMYYYY]\" adds a appointment to the appointment list!");
-        System.out.println("\"list [Doctor ID]\" brings up the list of current appointments for the doctor!");
-        System.out.println("\"delete [Appointment ID]\" deletes the appointment with the indicated ID from the list!");
-        System.out.println("\"help\" brings up a list of commands!");
-        System.out.println("\"return\" returns you to the Start Menu!");
+        System.out.println("Here is a list of Doctor Appointments Commands: ");
+
+        int[] lengthPara = {10, 70, 50};
+        printer(new String[]{HELP_HEADER_COMMAND, HELP_HEADER_DESCRIPTION, HELP_HEADER_FORMAT}, lengthPara);
+        UI.showLongLine();
+        printer(new String[]{HELP_COMMAND, APPOINTMENTS_HELP_DESCRIPTION, MARK_BLANK}, lengthPara);
+        printer(new String[]{ADD_COMMAND, APPOINTMENTS_ADD_DESCRIPTION, APPOINTMENTS_ADD_FORMAT}, lengthPara);
+        printer(new String[]{LIST_COMMAND, APPOINTMENTS_LIST_DESCRIPTION, APPOINTMENTS_LIST_FORMAT}, lengthPara);
+        printer(new String[]{DELETE_COMMAND, APPOINTMENTS_DELETE_DESCRIPTION, APPOINTMENTS_DELETE_FORMAT}, lengthPara);
+        printer(new String[]{RETURN_COMMAND, RETURN_DESCRIPTION, MARK_BLANK}, lengthPara);
+        UI.printEmptyLine();
     }
 
     public void printAppointmentMenuPrompt() {
@@ -35,6 +42,7 @@ public class DoctorAppointmentUI extends UI {
     public static void deletedDocID(String DocID, String AptID) {
         System.out.println("DoctorID / Appointment ID: " + DocID + "/" + AptID + " has been deleted!");
     }
+
     public static void deletedAptID(String AptID) {
         System.out.println("Appointment ID: " + AptID + " has been deleted!");
     }
@@ -49,6 +57,18 @@ public class DoctorAppointmentUI extends UI {
         System.out.println("Date: " + doc.getDateFormat(doc.getDate()));
         System.out.print("\n");
 
+    }
+
+    public static void AptPrintList(DoctorAppointment doc, String indicator) {
+        if (indicator.equals("D")) {
+            System.out.println(
+                    UI.prettyPrint("Doctor ID", 10) + " | " + UI.prettyPrint("Appointment ID", 10) + " | " + UI.prettyPrint("Name", 10) + " | "
+                            + UI.prettyPrint("Gender", 10) + UI.prettyPrint("Date", 10));
+        } else {
+            System.out.println(
+                    UI.prettyPrint("Appointment ID", 10) + " | " + UI.prettyPrint("Name", 10) + " | "
+                            + UI.prettyPrint("Gender", 10) + UI.prettyPrint("Date", 10));
+        }
     }
 
 }
