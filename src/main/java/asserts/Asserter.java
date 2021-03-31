4edf659164c73a584ed27db512e2784daaf304d1
cloudy3 <cjingfeng98@gmail.com>
2021-03-31 11:54:18 +0800

    Add assertions for positive vacancies and non-null employees

--- src/main/java/asserts/Asserter.java
+++ src/main/java/asserts/Asserter.java
@@ -1,4 +1,20 @@
 package asserts;
 
+import employee.Employee;
+
+import java.util.ArrayList;
+
 public class Asserter {
+    public static void assertIncrementScheduleSize(int originalScheduleLength, Employee person) {
+        assert originalScheduleLength == person.getSchedules().size() + 1: "1 schedule " +
+                "is supposed to be dropped.";
+    }
+
+    public static void assertPositiveVacancies(int vacancy) {
+        assert vacancy > 0;
+    }
+
+    public static void assertNonNullEmployees(ArrayList<Employee> employees) {
+        assert employees != null;
+    }
 }
