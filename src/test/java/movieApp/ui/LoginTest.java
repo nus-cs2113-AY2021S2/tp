40da12aee175fa23a0b75f5e6b81cabb4d0caaa5
Zufiqqar <solomiotrio@gmail.com>
2021-03-31 23:12:25 +0800

    Update LoginTest

--- src/test/java/movieApp/ui/LoginTest.java
+++ src/test/java/movieApp/ui/LoginTest.java
@@ -16,10 +16,11 @@ class LoginTest {
     @Test
     void TestLogin() throws Exception {
         ArrayList<User> users = new ArrayList<>();
-        users.add(new Admin("zul", "LgTVP/ep+g6U0Bx9DDSQvw==~3vlnbgaXMv92c9PNMY5iUA=="));
+        users.add(new Admin("IRVIN", "LgTVP/ep+g6U0Bx9DDSQvw==~3vlnbgaXMv92c9PNMY5iUA=="));
         users.add(new Customer("HELLO", "bTf+0p5Kownivvj2Q9ffeA==~liBWY/KKtUmUT9w5NYOyww=="));
 
-        String input = "zul\nLgTVP/ep+g6U0Bx9DDSQvw==~3vlnbgaXMv92c9PNMY5iUA==";
+
+        String input = "IRVIN\npassword1";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
 
