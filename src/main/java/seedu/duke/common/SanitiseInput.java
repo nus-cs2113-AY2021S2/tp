30403f3b3e2fbd8210eeef7caeba49a547027c55
Hemrish Bundhoo <e0407840@u.nus.edu>
2021-04-01 14:58:03 +0800

    Fix typo and use contains instead

--- src/main/java/seedu/duke/common/SanitiseInput.java
+++ src/main/java/seedu/duke/common/SanitiseInput.java
@@ -8,10 +8,9 @@ public class SanitiseInput {
 
         ArrayList<String> invalidCharacters = new ArrayList<>();
         initialiseInvalidCharactersList(invalidCharacters);
-        String[] words;
+
         for (String invalidCharacter : invalidCharacters) {
-            words = input.split(invalidCharacter);
-            if (words.length > 0) {
+            if (input.contains(invalidCharacter)) {
                 return true;
             }
         }
@@ -20,7 +19,7 @@ public class SanitiseInput {
 
     private static void initialiseInvalidCharactersList(ArrayList<String> invalidCharacters) {
         invalidCharacters.add("/");
-        invalidCharacters.add("\\\\");
+        invalidCharacters.add("\\");
         invalidCharacters.add(":");
         invalidCharacters.add("*");
         invalidCharacters.add("?");
