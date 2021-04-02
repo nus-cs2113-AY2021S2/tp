16567c04b742b043a3bf49550220bc6f4d95c001
nagiteja <sainagi9@gmail.com>
2021-04-02 12:38:58 +0800

    Add Time Stamp

--- src/main/java/reviews/Review.java
+++ src/main/java/reviews/Review.java
@@ -1,8 +1,13 @@
 package reviews;
 
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Review {
     private double rating = 0.0;
     private String description;
+    private String date;
 
     public Review(String description) {
         this.description = description;
@@ -11,6 +16,15 @@ public class Review {
     public Review(String description,double rating) {
         this.description = description;
         this.rating = rating;
+        Date dateTime = new Date();
+        Format formatter = new SimpleDateFormat("yyy-MM-dd");
+        this.date = formatter.format(dateTime);
+    }
+
+    public Review(String description,double rating,String date) {
+        this.description = description;
+        this.rating = rating;
+        this.date = date;
     }
 
     @Override
@@ -24,5 +38,9 @@ public class Review {
 
     public double getRating() {
         return rating;
+    }
+
+    public String getDate() {
+        return date;
     }
 }
