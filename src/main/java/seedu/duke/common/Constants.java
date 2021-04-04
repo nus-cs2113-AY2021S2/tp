34bdfb84f6104b4069d54890b23eaab66de1c413
isaharon <isabharon@gmail.com>
2021-04-04 20:51:12 +0800

    Update email regex for strict handling
    Follows RFC 5322 official standard

--- src/main/java/seedu/duke/common/Constants.java
+++ src/main/java/seedu/duke/common/Constants.java
@@ -84,7 +84,13 @@ public class Constants {
 
     //Parser
     public static final String FORMAT_MODULE_CODE = "([A-z]{2,3}[\\d]{4}[A-z]{0,2})";
-    public static final String FORMAT_EMAIL = "[A-z0-9_.-]+@[A-z.]+";
+    // General Email Regex (RFC 5322 Official Standard) taken from https://emailregex.com/
+    public static final String FORMAT_EMAIL = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\."
+            + "[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\"
+            + "[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]"
+            + "*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]"
+            + "?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\"
+            + "[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
     public static final String FORMAT_LINK = "[(http(s)?):\\/\\/(www\\.)?a-zA-Z0-9@:%._\\-\\+~#=]{2,256}\\.[a-z]{2,6}"
             + "\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)";
 
