af4a5ecfe62908a9aa4208e68259b137e8925df9
e0699194 <e0699194@u.nus.edu>
2021-04-06 15:22:18 +0800

    Added Menu class for showLoopingMenuUntiExit

--- src/main/java/seedu/duke/Controller.java
+++ src/main/java/seedu/duke/Controller.java
@@ -1,2 +1,52 @@
-package seedu.duke;public class Controller {
+package seedu.duke;
+
+public class Controller {
+
+	 Ui ui = new Ui();
+	 Parser parser = new Parser();
+	public void controllerForCommandsOnly(String userCommand, Deliveryman deliveryman){
+		switch (userCommand) {
+		case "help":
+			ui.showHelpMessage();
+			break;
+		case "profile":
+			ui.showProfile(deliveryman);
+			break;
+		case "start":
+			DeliveryList.load();
+			break;
+		case "list":
+			ui.showDeliveryList();
+			break;
+		case "record":
+			ui.showRecords(deliveryman.getRecords());
+			break;
+		case "route":
+		case "deliveryroute":
+			ui.processDeliveryRoute();
+			break;
+		case "bye":
+			ui.showFarewellScreen();
+			break;
+		default:
+			System.out.println("Please enter a valid command!");
+		}
+	}
+
+	public void controllerForCommandsAndArguments(String userCommand, String userArguments, Deliveryman deliveryman){
+		switch (userCommand){
+		case "edit":
+			String inputProfileData = parser.parseInput("edit", userArguments,deliveryman);
+			deliveryman.updateProfile(inputProfileData);
+			break;
+		case "view":
+			ui.processViewDelivery(userArguments, deliveryman, parser);
+			break;
+		case "complete":
+			ui.processCompleteDelivery(userArguments, deliveryman, parser);
+			break;
+		default:
+			System.out.println("Please enter a valid command!");
+		}
+	}
 }
--- src/main/java/seedu/duke/Menu.java
+++ src/main/java/seedu/duke/Menu.java
@@ -1,2 +1,32 @@
-package seedu.duke;public class Menu {
+package seedu.duke;
+
+import java.util.Scanner;
+
+public class Menu {
+	/**
+	 * Method backbone for menu selection
+	 * Parser is only called for commands that require argument parsing
+	 * @param deliveryman is the currently loaded profile
+	 */
+	public void showLoopingMenuUntilExit(Deliveryman deliveryman) {
+		Parser parser = new Parser();
+		Ui ui = new Ui();
+		Scanner sc = new Scanner(System.in);
+		String userInput, userCommand, userArguments;
+		Controller controller =  new Controller();
+		do {
+			ui.promptUserInput();
+			userInput = sc.nextLine();
+			if (userInput.isBlank()){
+				userInput = "INVALID";
+			}
+			userCommand = parser.parseCommand(userInput);
+			userArguments = parser.parseArguments(userInput);
+			if (!userArguments.isEmpty()){
+				controller.controllerForCommandsAndArguments(userCommand, userArguments, deliveryman);
+			} else{
+				controller.controllerForCommandsOnly(userCommand,deliveryman);
+			}
+		} while (!userCommand.equalsIgnoreCase("bye"));
+	}
 }
