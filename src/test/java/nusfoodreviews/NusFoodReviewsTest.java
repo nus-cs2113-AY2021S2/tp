3ad22a349b6d42819a136d94a64200eea36a75b4
KevinNg <WNG052@e.ntu.edu.sg>
2021-04-07 16:02:09 +0800

    Added tests for main

--- src/test/java/nusfoodreviews/NusFoodReviewsTest.java
+++ src/test/java/nusfoodreviews/NusFoodReviewsTest.java
@@ -2,19 +2,23 @@ package nusfoodreviews;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Scanner;
 
 class NusFoodReviewsTest {
-    private NusFoodReviews nusFoodReviews;
+    private NusFoodReviews nusFoodReviews = null;
 
-    @BeforeEach
-    public void initializeNusFoodReviews() throws IOException {
+    @AfterEach
+    public void tearDown() {
+        nusFoodReviews = null;
+    }
+
+    public void initializeNusFoodReviews(String input) throws IOException {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
         InputStream inputStream = NusFoodReviews.class.getClassLoader().getResourceAsStream("storage.txt");
         InputStreamReader streamReader = new InputStreamReader(inputStream);
         BufferedReader reader = new BufferedReader(streamReader);
@@ -22,7 +26,8 @@ class NusFoodReviewsTest {
     }
 
     @Test
-    public void resetAllIndexes_null_negatives() {
+    public void resetAllIndexes_null_negatives() throws IOException {
+        initializeNusFoodReviews("");
         nusFoodReviews.resetAllIndex();
         assertTrue(nusFoodReviews.getUserIndex() == -1);
         assertTrue(nusFoodReviews.getCanteenIndex() == -1);
@@ -30,9 +35,18 @@ class NusFoodReviewsTest {
     }
 
     @Test
-    public void resetCanteenStoreIndex_null_negatives() {
+    public void resetCanteenStoreIndex_null_negatives() throws IOException {
+        initializeNusFoodReviews("");
         nusFoodReviews.resetCanteenStoreIndex();
         assertTrue(nusFoodReviews.getCanteenIndex() == -1);
         assertTrue(nusFoodReviews.getStoreIndex() == -1);
+    }
+
+    @Test
+    void chooseUser_one_publicUser() throws IOException {
+        initializeNusFoodReviews("1\r\n");
+
+        int output = nusFoodReviews.chooseUser();
+        assertTrue(output == 0);
     }
 }
