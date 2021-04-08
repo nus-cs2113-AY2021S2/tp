f43a66d086f1cf2cee2399b5b90aa4df02e1b213
LAPTOP-VRGPN0FU\MingS <e0425961@u.nus.edu>
2021-04-09 00:42:13 +0800

    Logging for doctor appointment added + list/all function updated

--- src/main/java/seedu/logic/errorchecker/DoctorAppointmentChecker.java
+++ src/main/java/seedu/logic/errorchecker/DoctorAppointmentChecker.java
@@ -5,6 +5,7 @@ import seedu.exceptions.*;
 import seedu.exceptions.doctorappointment.*;
 import seedu.exceptions.doctorappointment.InvalidGenderException;
 import seedu.exceptions.IllegalCharacterException;
+import seedu.logger.HealthVaultLogger;
 import seedu.model.doctorappointment.AppointmentList;
 import seedu.model.doctorappointment.DoctorAppointment;
 import seedu.model.staff.Staff;
@@ -12,14 +13,13 @@ import seedu.storage.DoctorAppointmentStorage;
 import seedu.ui.UI;
 
 import java.io.FileNotFoundException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.time.format.ResolverStyle;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DoctorAppointmentChecker extends MainChecker {
     private static String doctorID;
@@ -28,6 +28,7 @@ public class DoctorAppointmentChecker extends MainChecker {
     private static String ID;
     private static String gender;
     private static String date;
+    public static Logger logger = HealthVaultLogger.getLogger();
 
     public static void checkValidDataForAdd(String[] input) throws HealthVaultException {
         doctorID = input[1];
@@ -35,6 +36,7 @@ public class DoctorAppointmentChecker extends MainChecker {
         name = input[3];
         gender = input[4];
         date = input[5];
+        logger.log(Level.INFO, "Checking for Valid data after add command");
         if (!isValidDocID(doctorID)) {
             throw new DocIDNotFoundException(doctorID);
         }
@@ -53,6 +55,7 @@ public class DoctorAppointmentChecker extends MainChecker {
 
         if (AppointmentList.appointmentList.size() ==0) throw new EmptyListException();
         if (ID.equals("all")) return;
+        logger.log(Level.INFO, "Checking for Valid data after list command");
         if (!isValidDocID(ID) && !isValidListAppointmentID(ID)) {
             throw new seedu.exceptions.InvalidIDException();
         }
@@ -60,6 +63,7 @@ public class DoctorAppointmentChecker extends MainChecker {
 
     public static void checkValidDataForDelete(String[] input) throws seedu.exceptions.InvalidIDException {
         ID = input[1];
+        logger.log(Level.INFO, "Checking for Valid data after delete command");
         if (!isValidIDToDelete(ID)) {
             throw new seedu.exceptions.InvalidIDException();
         }
@@ -68,6 +72,7 @@ public class DoctorAppointmentChecker extends MainChecker {
     public static void checkAptID(String id) throws WrongAptIDFormatException {
         try {
             Integer.parseInt(id.substring(1));
+            logger.log(Level.INFO, "Checking for Valid Appointment ID");
         } catch (NumberFormatException e) {
             throw new WrongAptIDFormatException();
         }
@@ -84,6 +89,7 @@ public class DoctorAppointmentChecker extends MainChecker {
     }
 
     public static void illegalCharacterNameCheckerForStorage(String name) throws CorruptedFileException {
+        logger.log(Level.INFO, "Checking for Illegal Character for Name from storage data ");
         String cleanedInput = UI.cleanseInput(name);
         if (!name.equals(cleanedInput)) {
             throw new CorruptedFileException(Constants.APPOINTMENT_FILE_PATH);
@@ -97,6 +103,7 @@ public class DoctorAppointmentChecker extends MainChecker {
         illegalCharacterNameCheckerForStorage(inputArray[1]);
         checkDuplicateAptIDFromStorage(inputArray[1], storageList);
         checkValidDate(inputArray[4]);
+        logger.log(Level.INFO, "Checking Validity of storage data ");
         if (!isValidGender(inputArray[3])) {
             throw new CorruptedFileException(Constants.APPOINTMENT_FILE_PATH);
         }
@@ -112,6 +119,7 @@ public class DoctorAppointmentChecker extends MainChecker {
 
     public static void checkID(String doctorID, String appointmentID) throws HealthVaultException {
         try {
+            logger.log(Level.INFO, "Checking Validity Doctor and Appointment ID from storage data ");
             Integer.parseInt(doctorID.substring(1));
             Integer.parseInt(appointmentID.substring(1));
         } catch (NumberFormatException e) {
@@ -128,7 +136,7 @@ public class DoctorAppointmentChecker extends MainChecker {
     public static boolean isValidDocID(String doctorID) {
         try {
             String[] character = doctorID.split("");
-
+            logger.log(Level.INFO, "Checking Validity Doctor ID during program commands ");
             if (character[0].equals("D")) {
                 ArrayList<Staff> doctorList;
                 doctorList = DoctorAppointmentStorage.loadDoctorFile();
@@ -149,6 +157,7 @@ public class DoctorAppointmentChecker extends MainChecker {
         String[] character = appointmentID.split("");
 
         checkAptID(appointmentID);
+        logger.log(Level.INFO, "Checking Validity Appointment ID during ADD command ");
         illegalCharacterChecker(appointmentID, "Appointment ID");
         if (character[0].equals("A")) {
             for (DoctorAppointment id : AppointmentList.appointmentList) {
@@ -163,6 +172,7 @@ public class DoctorAppointmentChecker extends MainChecker {
 
     public static boolean isValidListAppointmentID(String appointmentID) {
         String[] character = appointmentID.split("");
+        logger.log(Level.INFO, "Checking Validity Appointment ID during LIST command ");
 
         if (character[0].equals("A")) {
             for (DoctorAppointment id : AppointmentList.appointmentList) {
@@ -176,11 +186,13 @@ public class DoctorAppointmentChecker extends MainChecker {
 
 
     public static boolean isValidGender(String gender) {
+        logger.log(Level.INFO, "Checking Validity of gender");
         return gender.equals("M") || gender.equals("F");
     }
 
     public static void checkValidDate(final String date) throws InvalidDateException {
         try {
+            logger.log(Level.INFO, "Checking Validity of date");
             LocalDate.parse(date,
                     DateTimeFormatter.ofPattern("ddMMuuuu")
                             .withResolverStyle(ResolverStyle.STRICT));
@@ -191,6 +203,7 @@ public class DoctorAppointmentChecker extends MainChecker {
 
     public static boolean isValidIDToDelete(String ID) {
         String[] IDKeyword = ID.split("");
+        logger.log(Level.INFO, "Checking Validity of Doctor/ Appointment ID to be deleted");
 
         for (DoctorAppointment doc : AppointmentList.appointmentList) {
             if (IDKeyword[0].equals("A")) {
