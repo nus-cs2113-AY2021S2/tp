a8077eef53c6780f6a068468fc692deb83588a90
kangxinwang <juliawangkangxin@gmail.com>
2021-04-08 11:57:59 +0800

    add message to inform users no canteen to delete and reviews to read

--- src/main/java/command/DeleteCanteenCommand.java
+++ src/main/java/command/DeleteCanteenCommand.java
@@ -11,6 +11,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
 
+import static ui.Ui.LINESPACING;
+
 public class DeleteCanteenCommand extends Command {
 
     private Parser parser;
@@ -23,18 +25,24 @@ public class DeleteCanteenCommand extends Command {
 
     @Override
     public void execute(ArrayList<Canteen> canteens, Ui ui) throws IOException, DukeExceptions {
-        ui.showDisplaySelectCanteens(canteens, "delete");
         int numCanteens = canteens.size();
-        String line = ui.readCommand();
-        if (line.equals("cancel")) {
-            ui.showCanteenNotDeleted();
-            return;
-        }
-        int canteenIndex = parser.parseInt(line, Math.min(1, numCanteens), numCanteens) - 1;
+        if(numCanteens>0){
+            ui.showDisplaySelectCanteens(canteens, "delete");
+            String line = ui.readCommand();
+            if (line.equals("cancel")) {
+                ui.showCanteenNotDeleted();
+                return;
+            }
+            int canteenIndex = parser.parseInt(line, Math.min(1, numCanteens), numCanteens) - 1;
 
-        assert canteenIndex >= 0 && canteenIndex < canteens.size() : "DeleteCanteenCommand canteenIndex invalid";
-        Canteen removedCanteen = canteens.remove(canteenIndex);
-        ui.showCanteenDeleted(removedCanteen, canteens.size());
-        Storage.save(new FileWriter(savePath),canteens);
+            assert canteenIndex >= 0 && canteenIndex < canteens.size() : "DeleteCanteenCommand canteenIndex invalid";
+            Canteen removedCanteen = canteens.remove(canteenIndex);
+            ui.showCanteenDeleted(removedCanteen, canteens.size());
+            Storage.save(new FileWriter(savePath),canteens);
+        } else{
+            System.out.println("There are no canteens left!");
+            System.out.println(LINESPACING);
+        }
+
     }
 }
