1243a4ed4cb165c1fe82aaa0b0ddaeae60cd91f0
blank-bank <yipowen@yahoo.com.sg>
2021-04-11 12:57:14 +0800

    More minor checkstyle

--- src/test/java/seedu/staff/StaffCheckerTest.java
+++ src/test/java/seedu/staff/StaffCheckerTest.java
@@ -37,9 +37,9 @@ public class StaffCheckerTest {
     }
 
     @Test
-    public void checkStaffId_negativeNumberSectionInId_WrongStaffIDExceptionReturned() {
+    public void checkStaffId_negativeNumberSectionInId_WrongStaffIdExceptionReturned() {
         WrongStaffIdException exception = assertThrows(WrongStaffIdException.class,
-                () -> staffChecker.checkStaffID("D-1234"));
+            () -> staffChecker.checkStaffID("D-1234"));
 
         assertEquals(exception.getMessage(), "Error in Staff ID input\n"
                 + "Please input with the following format [D/N][5 digit ID number]");
@@ -49,7 +49,7 @@ public class StaffCheckerTest {
     public void checkStaffDataForStorage_insufficientStaffID_HealthVaultReturned() {
         ArrayList<Staff> list = new ArrayList<>();
         HealthVaultException exception = assertThrows(HealthVaultException.class,
-                () -> staffChecker.checkValidDataFromStorage("D1234|Owen|23|Surgeon", list));
+            () -> staffChecker.checkValidDataFromStorage("D1234|Owen|23|Surgeon", list));
 
         assertEquals(exception.getMessage(), "Error in Staff ID input\n"
                 + "Please input with the following format [D/N][5 digit ID number]");
@@ -59,7 +59,7 @@ public class StaffCheckerTest {
     public void checkStaffDataForStorage_blankInput_HealthVaultReturned() {
         ArrayList<Staff> list = new ArrayList<>();
         HealthVaultException exception = assertThrows(HealthVaultException.class,
-                () -> staffChecker.checkValidDataFromStorage("D1234| |23|Surgeon", list));
+            () -> staffChecker.checkValidDataFromStorage("D1234| |23|Surgeon", list));
 
         assertEquals(exception.getMessage(), "Error in Staff ID input\n"
                 + "Please input with the following format [D/N][5 digit ID number]");
