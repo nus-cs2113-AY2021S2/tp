141d1f905ee533ecab02b97450050ada9d7c2315
Rizavur Rahman <rizavur@gmail.com>
2021-04-12 01:33:45 +0800

    Add UI java docs

--- src/main/java/seedu/duke/ui/FavouriteUi.java
+++ src/main/java/seedu/duke/ui/FavouriteUi.java
@@ -7,10 +7,22 @@ import seedu.duke.exception.SameBlockException;
 
 public class FavouriteUi extends UiManager {
 
+    /**
+     * This method gets the routing information to be saved as favourites by calling the router ui class.
+     * @return A pair of starting and destination blocks are returned, where starting block is stored in the first index of
+     * the array and the destination block is stored in the second index of the array.
+     * @throws SameBlockException If the same start and destination blocks are entered by the user.
+     */
     public String[] getFavouriteInfo() throws SameBlockException {
         return new RouterUi().getRoutingInfo();
     }
 
+    /**
+     * This method lists out all the favourite routes that have been stored by the user.
+     * @param favourite The instance of the favourites data model.
+     * @throws EmptyFavouriteException If there are no favourites stored.
+     * @throws InvalidIndexException If the specified index in the favourites list does not exist.
+     */
     public void showFavourites(Favourite favourite) throws EmptyFavouriteException, InvalidIndexException {
         if (favourite.isEmpty()) {
             throw new EmptyFavouriteException();
@@ -24,10 +36,20 @@ public class FavouriteUi extends UiManager {
         }
     }
 
+    /**
+     * This method gets the index of the favourites entry that the user wants to delete.
+     * @return The index number of the favourites entry that is to be deleted.
+     * @throws InvalidIndexException If the index to be deleted is not an integer that is within the bounds.
+     */
     public int getDeleteIndex() throws InvalidIndexException {
         return new NoteUi().getDeleteIndex();
     }
 
+    /**
+     * This method gets the index of the favourites entry that the user wants to repeat.
+     * @return The index number of the favourites entry that is to be repeated.
+     * @throws InvalidIndexException If the index to be deleted is not an integer that is within the bounds.
+     */
     public int getRepeatIndex() throws InvalidIndexException {
         return new HistoryUi().getRepeatIndex();
     }
