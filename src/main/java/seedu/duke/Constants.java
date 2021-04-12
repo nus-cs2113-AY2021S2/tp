769209c57f710af8cda5bb05a5ce274205086290
Emkay16 <mabelkohjx@gmail.com>
2021-04-12 20:56:03 +0800

    Update help messages

--- src/main/java/seedu/duke/Constants.java
+++ src/main/java/seedu/duke/Constants.java
@@ -9,43 +9,43 @@ public class Constants {
     // Help messages
     public static final String COMMAND_LIST_MESSAGE = "Here is a list of available commands:\n"
             + "  add\n  delete\n  list\n  load\n  record\n  retrieve\n  current\n  exit\n"
-            + "Type 'help' followed by one or more commands for more details\n"
+            + "Type 'help' followed by one or more commands for more details.\n"
             + "Example: help record";
-    public static final String ADD_INFO_MESSAGE = "Add a patient to the list\n"
+    public static final String ADD_INFO_MESSAGE = "Add a patient to the list.\n"
             + "Command prefix: add\n"
             + "Argument(s): IC number\n"
             + "Usage: add IC_NUMBER\n"
             + "Example: add S1234567D\n";
-    public static final String DELETE_INFO_MESSAGE = "Delete a patient from the list or delete a patient's "
-            + "consultation details for a specific date\n"
-            + "Command prefix: delete\n"
-            + "Argument(s): IC number or date\n"
-            + "Usage: delete [/p IC_NUMBER]\n"
-            + "Example: delete /p S1234567D\n"
-            + "Usage: delete [/r DATE]\n"
+    public static final String DELETE_INFO_MESSAGE = "Delete a patient from the list.\n"
+            + "Argument(s): IC number\n"
+            + "Usage: delete /p IC_NUMBER\n"
+            + "Example: delete /p S1234567D\n\n"
+            + "Delete a record from the list of records for the currently loaded patient.\n"
+            + "Argument(s): Date\n"
+            + "Usage: delete /r DATE\n"
             + "Example: delete /r 26/03/2021\n";
-    public static final String LIST_INFO_MESSAGE = "Show the list of all patients\n"
+    public static final String LIST_INFO_MESSAGE = "Show the list of all patients.\n"
             + "Usage: list\n";
-    public static final String LOAD_INFO_MESSAGE = "Select a specified patient to add and retrieve records\n"
+    public static final String LOAD_INFO_MESSAGE = "Select a specified patient to add and retrieve records.\n"
             + "Command prefix: load\n"
             + "Argument(s): IC number\n"
             + "Usage: load IC_NUMBER\n"
             + "Example: load S1234567D\n";
-    public static final String RECORD_CONSULTATION_INFO_MESSAGE = "Add a consultation record to the selected patient\n"
+    public static final String RECORD_CONSULTATION_INFO_MESSAGE = "Add a consultation record to the selected patient.\n"
             + "Command prefix: record\n"
             + "Arguments(s): consultation details, symptom, diagnosis, prescription\n"
             + "Usage: record [CONSULTATION_DATE] [/s SYMPTOM] [/d DIAGNOSIS] [/p PRESCRIPTION]\n"
             + "Example: record /s fever\n";
-    public static final String RETRIEVE_INFO_MESSAGE = "Retrieve past consultation record from the selected patient\n"
+    public static final String RETRIEVE_INFO_MESSAGE = "Retrieve past consultation record from the selected patient.\n"
             + "Usage: retrieve\n";
     public static final String CURRENT_INFO_MESSAGE = "Print out the current patient being loaded.\n"
             + "Usage: current\n";
-    public static final String HELP_INFO_MESSAGE = "Displays a list of commands or information of selected commands\n"
+    public static final String HELP_INFO_MESSAGE = "Displays a list of commands or information of selected commands.\n"
             + "Command prefix: help\n"
             + "Argument(s): commands\n"
             + "Usage: help [OPTIONAL_COMMANDS]\n"
             + "Example: help list add\n";
-    public static final String EXIT_INFO_MESSAGE = "Exit the program\n"
+    public static final String EXIT_INFO_MESSAGE = "Exit the program.\n"
             + "Usage: exit\n";
 
     public static final String INVALID_COMMAND_MESSAGE = "Invalid command: %s";
