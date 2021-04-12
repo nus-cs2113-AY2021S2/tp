ab95cbadaefaea09c3e00a7fdc363079828dfe1e
wjchoi0712 <e0376605@u.nus.edu>
2021-04-12 17:58:53 +0800

    Fixed bug for alias and note

--- src/main/java/seedu/duke/exception/InvalidNoteException.java
+++ src/main/java/seedu/duke/exception/InvalidNoteException.java
@@ -3,6 +3,6 @@ package seedu.duke.exception;
 public class InvalidNoteException extends NusMazeException {
     @Override
     public String getMessage() {
-        return "Your note description cannot be empty! Please try the command again :)";
+        return "Your note description cannot be empty or contain \"/\". Please try the command again :)";
     }
 }
